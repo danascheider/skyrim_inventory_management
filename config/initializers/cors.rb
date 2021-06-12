72321fcb21f6be5c032285b2638754d3bcf91122
Dana Scheider <dana.scheider@envato.com>
2021-06-12 22:02:28 +1000

    Configure CORS to allow requests from the front end

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Be sure to restart your server when you modify this file.
 
 # Avoid CORS issues when API is called from the frontend app.
@@ -5,12 +7,14 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+allowed_origin = Rails.env.production? 'https://sim.danascheider.com' : ENV['FRONTEND_ORIGIN']
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins allowed_origin
+
+    resource '*',
+      headers: :any,
+      methods: %i[get post put patch delete options head]
+  end
+end
