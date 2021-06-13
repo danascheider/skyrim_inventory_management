c03d75aaae991067346468d3b141c228092ea926
Dana Scheider <dana.scheider@envato.com>
2021-06-13 10:32:26 +1000

    Revert "WIP commit - devise_token_auth:install - revert to here if there is an issue after I make changes"
    This reverts commit 7371633acf45641b7358b6482bc60c434c770047.

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, configatron.google_oauth2_key, configatron.google_oauth2_secret, scope: 'email'
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
   root to: 'health_checks#index'
 
   get '/privacy', to: 'utilities#privacy'
