7371633acf45641b7358b6482bc60c434c770047
Dana Scheider <dana.scheider@envato.com>
2021-06-13 10:27:30 +1000

    WIP commit - devise_token_auth:install - revert to here if there is an issue after I make changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   root to: 'health_checks#index'
 
   get '/privacy', to: 'utilities#privacy'
