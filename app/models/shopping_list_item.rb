177bfd2c5dc6951c8fe6566d09c6709153e06cbd
Dana Scheider <dana.scheider@envato.com>
2021-06-21 08:03:47 +1000

    Update notes when item destroyed

--- app/models/shopping_list_item.rb
+++ app/models/shopping_list_item.rb
@@ -15,9 +15,9 @@ class ShoppingListItem < ApplicationRecord
   delegate :user, to: :shopping_list
 
   def self.create_or_combine!(attrs)
-    list = attrs[:shopping_list] || ShoppingList.find(attrs[:shopping_list_id])
+    shopping_list = attrs[:shopping_list] || ShoppingList.find(attrs[:shopping_list_id])
     desc = (attrs[:description] || attrs['description'])&.humanize
-    existing_item = list.shopping_list_items.find_by_description(desc)
+    existing_item = shopping_list.shopping_list_items.find_by_description(desc)
 
     if existing_item.nil?
       create!(attrs)
@@ -45,9 +45,12 @@ class ShoppingListItem < ApplicationRecord
     # The new value will never be zero because there will be a validation error before saving if the
     # new quantity value is zero. On the client side, when a user enters a quantity of zero, the client
     # should implement logic to make a DELETE request on the list item instead.
-    delta_quantity = saved_change_to_attribute(:quantity).last - saved_change_to_attribute(:quantity).first
     item_on_master_list = master_list.shopping_list_items.find_by_description(description)
-    item_on_master_list.update!(quantity: item_on_master_list.quantity + delta_quantity)
+
+    item_on_master_list.update!(
+      quantity: item_on_master_list.quantity + delta_quantity,
+      notes: update_combined_note_values(item_on_master_list.notes, *saved_change_to_attribute(:notes))
+    )
   end
 
   def adjust_master_list_after_destroy
@@ -56,7 +59,16 @@ class ShoppingListItem < ApplicationRecord
     if item_on_master_list.quantity == quantity
       item_on_master_list.destroy!
     else
-      item_on_master_list.update!(quantity: item_on_master_list.quantity - quantity)
+      if notes.present?
+        new_notes = item_on_master_list.notes.gsub(/#{notes}/, '').gsub(/^ ?\-\- /, '').gsub(/ \-\- ?$/, '')
+      end
+
+      new_notes = nil unless defined?(new_notes) && new_notes.present?
+
+      item_on_master_list.update!(
+        quantity: item_on_master_list.quantity - quantity,
+        notes: new_notes || item_on_master_list.notes
+      )
     end
   end
 
@@ -70,6 +82,34 @@ class ShoppingListItem < ApplicationRecord
 
   def shopping_list_is_master_list?
     self.shopping_list.master == true
+  end
+
+  def delta_quantity
+    saved_change_to_attribute(:quantity).present? ? 
+      saved_change_to_attribute(:quantity).last - saved_change_to_attribute(:quantity).first :
+      0
+  end
+
+  # When updating the notes on a regular list item, we also want to update
+  # the value on the corresponding master list. The issue is that the notes
+  # on a master list item consists of the combined notes fields of all regular
+  # list items matching that description. We only want to update the note
+  # that's being changed.
+  #
+  # All args to this method are strings. "combined_value" is the existing
+  # value of the notes on the master list. "old_value" is the original value
+  # of the note being changed, and "new_value" is the new value. The "notes"
+  # field of the master list item is set to the returned value in the
+  # #adjust_master_list_after_update method.
+  #
+  def update_combined_note_values(combined_value = nil, old_value = nil, new_value = nil)
+    return new_value unless combined_value.present?
+
+    if old_value.present? && combined_value =~ /#{old_value}/
+      combined_value.sub(old_value, new_value.to_s).gsub(/^ ?\-\- /, '').gsub(/ \-\- ?$/, '')
+    elsif old_value.blank?
+      [combined_value, new_value].join(' -- ').gsub(/^ ?\-\- /, '').gsub(/ \-\- ?$/, '')
+    end
   end
 
   def master_list
