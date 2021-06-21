3ead0a6a2b2a74e0be48a00289fe975a5c6ebc5c
Dana Scheider <dana.scheider@envato.com>
2021-06-21 11:27:55 +1000

    Try another thing to fix DB connection in RSpec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,13 +25,11 @@ require 'rspec/rails'
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
 
-unless ENV['RAILS_ENV'] == 'ci'
-  begin
-    ActiveRecord::Migration.maintain_test_schema!
-  rescue ActiveRecord::PendingMigrationError => e
-    puts e.to_s.strip
-    exit 1
-  end
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
 end
 
 DatabaseCleaner.clean_with :transaction
