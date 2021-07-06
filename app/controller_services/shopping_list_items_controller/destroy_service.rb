68fd8974e41e98d7f5c222d6b1483fb8b3bd8e4e
Dana Scheider <dana.scheider@envato.com>
2021-07-06 14:39:55 +1000

    Create ShoppingListItemsController::DestroyService and add tests

---
+++ app/controller_services/shopping_list_items_controller/destroy_service.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'service/no_content_result'
+require 'service/ok_result'
+require 'service/not_found_result'
+require 'service/method_not_allowed_result'
+
+class ShoppingListItemsController < ApplicationController
+  class DestroyService
+    MASTER_LIST_ERROR = 'Cannot manually delete list item from master shopping list'
+
+    def initialize(user, item_id)
+      @user = user
+      @item_id = item_id
+    end
+
+    def perform
+      return Service::MethodNotAllowedResult.new(errors: [MASTER_LIST_ERROR]) if shopping_list_item.list.master == true
+
+      shopping_list_item.destroy!
+      master_list_item = master_list.remove_item_from_child_list(shopping_list_item.attributes)
+      master_list_item.nil? ? Service::NoContentResult.new : Service::OKResult.new(resource: master_list_item)
+    rescue ActiveRecord::RecordNotFound
+      Service::NotFoundResult.new
+    end
+
+    private
+
+    attr_reader :user, :item_id
+
+    def master_list
+      @master_list ||= shopping_list_item.list.master_list
+    end
+
+    def master_list_item
+      @master_list_item ||= master_list.list_items.find_by(description: shopping_list_item.description)
+    end
+
+    def shopping_list_item
+      @shopping_list_item ||= user.shopping_list_items.find(item_id)
+    end
+  end
+end
---
+++ spec/controller_services/shopping_list_items_controller/destroy_service_spec.rb
@@ -0,0 +1,125 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'service/no_content_result'
+require 'service/ok_result'
+require 'service/not_found_result'
+require 'service/method_not_allowed_result'
+
+RSpec.describe ShoppingListItemsController::DestroyService do
+  describe '#perform' do
+    subject(:perform) { described_class.new(user, list_item.id).perform }
+
+    let(:user) { create(:user) }
+    let!(:master_list) { create(:master_shopping_list, user: user) }
+    let!(:shopping_list) { create(:shopping_list, user: user, master_list: master_list) }
+
+    context 'when all goes well' do
+      let(:list_item) { create(:shopping_list_item, list: shopping_list, notes: 'some notes') }
+
+      before do
+        master_list.add_item_from_child_list(list_item)
+      end
+
+      context 'when the quantity on the master list equals the quantity on the regular list' do
+        it 'destroys the list item' do
+          perform
+          expect { ShoppingListItem.find(list_item.id) }.to raise_error ActiveRecord::RecordNotFound
+        end
+
+        it 'destroys the item on the master list' do
+          expect { perform }.to change(master_list.list_items, :count).from(1).to(0)
+        end
+
+        it 'returns a Service::NoContentResult' do
+          expect(perform).to be_a Service::NoContentResult
+        end
+
+        it 'does not return data' do
+          expect(perform.resource).to be nil
+        end
+      end
+
+      context 'when the quantity on the master list exceed the quantity on the regular list' do
+        let(:second_list) { create(:shopping_list, user: user, master_list: master_list) }
+        let(:second_list_item) do
+          create(:shopping_list_item,
+                  list: second_list,
+                  description: list_item.description,
+                  quantity: 2,
+                  notes: 'some other notes'
+                )
+        end
+
+        before do
+          master_list.add_item_from_child_list(second_list_item)
+        end
+
+        it 'destroys the list item' do
+          perform
+          expect { ShoppingListItem.find(list_item.id) }.to raise_error ActiveRecord::RecordNotFound
+        end
+        
+        it 'changes the quantity of the master list item' do
+          perform
+          expect(master_list.list_items.first.quantity).to eq 2
+        end
+
+        it 'changes the notes of the master list item', :aggregate_failures do
+          perform
+          expect(master_list.list_items.first.notes).to match /some other notes/
+          expect(master_list.list_items.first.notes).not_to match /some notes/
+        end
+
+        it 'returns a Service::OKResult' do
+          expect(perform).to be_a Service::OKResult
+        end
+
+        it 'returns the updated master list item' do
+          expect(perform.resource).to eq master_list.list_items.first
+        end
+      end
+    end
+
+    context "when the specified list item doesn't exist" do
+      let(:list_item) { double("this item doesn't exist", id: 389) }
+
+      it 'returns a Service::NotFoundResult' do
+        expect(perform).to be_a(Service::NotFoundResult)
+      end
+
+      it "doesn't return any error messages" do
+        expect(perform.errors).to eq []
+      end
+    end
+
+    context "when the specified list item doesn't belong to the authenticated user" do
+      let(:list_item) { create(:shopping_list_item) }
+
+      it 'returns a Service::NotFoundResult' do
+        expect(perform).to be_a Service::NotFoundResult
+      end
+
+      it "doesn't return any error messages" do
+        expect(perform.errors).to eq []
+      end
+    end
+
+    context 'when the specified list item is on a master list' do
+      let(:list_item) { create(:shopping_list_item, list: master_list) }
+
+      it "doesn't destroy the list item" do
+        perform
+        expect(ShoppingListItem.find(list_item.id)).to be_a ShoppingListItem
+      end
+
+      it 'returns a Service::MethodNotAllowedResult' do
+        expect(perform).to be_a Service::MethodNotAllowedResult
+      end
+
+      it 'includes a helpful error message' do
+        expect(perform.errors).to eq ['Cannot manually delete list item from master shopping list']
+      end
+    end
+  end
+end
