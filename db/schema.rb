bcd3be4dc395b941511a0881a19cf54d0ca6c247
Dana Scheider <dana.scheider@envato.com>
2021-07-15 18:55:33 +1000

    Remove user_id column from shopping lists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_15_082312) do
+ActiveRecord::Schema.define(version: 2021_07_15_085102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 2021_07_15_082312) do
   end
 
   create_table "shopping_lists", force: :cascade do |t|
-    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "aggregate", default: false
@@ -42,7 +41,6 @@ ActiveRecord::Schema.define(version: 2021_07_15_082312) do
     t.integer "aggregate_list_id"
     t.bigint "game_id", null: false
     t.index ["game_id"], name: "index_shopping_lists_on_game_id"
-    t.index ["user_id", "title"], name: "index_shopping_lists_on_user_id_and_title", unique: true
   end
 
   create_table "users", force: :cascade do |t|
