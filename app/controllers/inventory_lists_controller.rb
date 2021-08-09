a0edc01ad99d8ef6bd59e99d344726e3c46d4839
Dana Scheider <dana.scheider@envato.com>
2021-08-09 17:31:54 +1000

    Make request specs pass for creating an inventory list

--- app/controllers/inventory_lists_controller.rb
+++ app/controllers/inventory_lists_controller.rb
@@ -8,4 +8,16 @@ class InventoryListsController < ApplicationController
 
     ::Controller::Response.new(self, result).execute
   end
+
+  def create
+    result = CreateService.new(current_user, params[:game_id], inventory_list_params).perform
+
+    ::Controller::Response.new(self, result).execute
+  end
+
+  private
+
+  def inventory_list_params
+    params[:inventory_list].present? ? params.require(:inventory_list).permit(:title, :aggregate) : {}
+  end
 end
