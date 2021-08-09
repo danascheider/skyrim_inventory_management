7e4b8b4e4f8d78708995cdde9659ea7f284873c8
Dana Scheider <dana.scheider@envato.com>
2021-08-09 15:40:41 +1000

    Create InventoryListsController::IndexService class to handle GET requests to /games/:game_id/inventory_lists

---
+++ spec/controller_services/inventory_lists_controller/index_service_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'service/ok_result'
+require 'service/not_found_result'
+require 'service/internal_server_error_result'
+
+RSpec.describe InventoryListsController::IndexService do
+  describe '#perform' do
+    subject(:perform) { described_class.new(user, game.id).perform }
+
+    let(:user) { create(:user) }
+
+    context 'when there are no inventory lists for that game' do
+      let(:game) { create(:game, user: user) }
+
+      it 'returns a Service::OKResult' do
+        expect(perform).to be_a(Service::OKResult)
+      end
+
+      it 'returns the empty list' do
+        expect(perform.resource).to eq []
+      end
+    end
+
+    context 'when there are inventory lists for that game' do
+      let(:game) { create(:game_with_inventory_lists, user: user) }
+
+      it 'returns a Service::OKResult' do
+        expect(perform).to be_a(Service::OKResult)
+      end
+
+      it 'returns the inventory lists' do
+        expect(perform.resource).to eq game.inventory_lists
+      end
+    end
+
+    context 'when the game is not found' do
+      let(:game) { double(id: 2389) }
+
+      it 'returns a Service::NotFoundResult' do
+        expect(perform).to be_a(Service::NotFoundResult)
+      end
+
+      it "doesn't return any error messages" do
+        expect(perform.errors).to be_empty
+      end
+    end
+
+    context "when the game doesn't belong to the user"
+
+    context 'when something unexpected goes wrong'
+  end
+end
