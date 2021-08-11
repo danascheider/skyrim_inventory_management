e8489ac79e32b64d83a9e5c4783b1d2b6b7f42ba
Dana Scheider <dana.scheider@envato.com>
2021-08-12 07:16:06 +1000

    Add more shopping list item update service tests to account for new behaviour around unit_weight

--- spec/controller_services/shopping_list_items_controller/update_service_spec.rb
+++ spec/controller_services/shopping_list_items_controller/update_service_spec.rb
@@ -3,172 +3,146 @@
 require 'rails_helper'
 require 'service/ok_result'
 require 'service/not_found_result'
-require 'service/method_not_allowed_result'
 require 'service/unprocessable_entity_result'
+require 'service/method_not_allowed_result'
 require 'service/internal_server_error_result'
 
 RSpec.describe ShoppingListItemsController::UpdateService do
   describe '#perform' do
     subject(:perform) { described_class.new(user, list_item.id, params).perform }
 
-    let(:user)           { create(:user) }
-    let!(:shopping_list) { game.shopping_lists.find_by(aggregate: false) }
+    let(:user)            { create(:user) }
+    let(:game)            { create(:game, user: user) }
+    let!(:aggregate_list) { create(:aggregate_shopping_list, game: game) }
+    let!(:shopping_list)  { create(:shopping_list, game: game, aggregate_list: aggregate_list) }
 
     context 'when all goes well' do
-      let(:game)           { create(:game_with_shopping_lists, user: user) }
-      let(:aggregate_list) { game.aggregate_shopping_list }
-      let!(:list_item)     { create(:shopping_list_item, list: shopping_list, quantity: 2, unit_weight: 2) }
-      let(:params)         { { quantity: 3 } }
-      let(:scope)          { ShoppingListItem.belonging_to_user(user) }
+      context 'when there is no matching item on another list' do
+        let!(:list_item)          { create(:shopping_list_item, list: shopping_list, description: 'Dwarven metal ingot', quantity: 2) }
+        let(:aggregate_list_item) { aggregate_list.list_items.first }
+        let(:params)              { { quantity: 9, notes: 'To make bolts with' } }
 
-      before do
-        aggregate_list.add_item_from_child_list(list_item)
-      end
+        before do
+          aggregate_list.add_item_from_child_list(list_item)
+        end
 
-      it 'updates the shopping list item', :aggregate_failures do
-        perform
-        expect(list_item.reload.quantity).to eq 3
-        expect(list_item.reload.notes).to be nil
-      end
-
-      it 'updates the item on the aggregate list' do
-        # Put the mocks in here because I don't want this much mocking in the other specs
-        allow(ShoppingListItem).to receive(:belonging_to_user).with(user).and_return(scope)
-        allow(scope).to receive(:find).and_return(list_item)
-        allow(list_item).to receive(:list).and_return(shopping_list)
-        allow(shopping_list).to receive(:aggregate_list).and_return(aggregate_list)
-        allow(aggregate_list).to receive(:update_item_from_child_list).and_call_original
-        perform
-        expect(aggregate_list).to have_received(:update_item_from_child_list).with(list_item.description, 1, nil, nil, nil)
-      end
-
-      it 'returns a Service::OKResult' do
-        expect(perform).to be_a(Service::OKResult)
-      end
-
-      it 'sets the resource to include both the regular and aggregate list item' do
-        expect(perform.resource).to eq([aggregate_list.list_items.reload.first, list_item])
-      end
-
-      it 'updates the updated_at timestamp on the list' do
-        t = Time.zone.now + 3.days
-        Timecop.freeze(t) do
+        it 'updates the list item', :aggregate_failures do
           perform
-          # This is another case of a rounding error in the CI environment. The server where
-          # GitHub Actions run seems to truncate the last few digits of the timestamp, resulting
-          # in things being not quite equal in that environment. Since it's not that important
-          # that it be that exact, I'm just using the `be_within` matcher.
-          expect(shopping_list.reload.updated_at).to be_within(0.005.seconds).of(t)
+          expect(list_item.reload.quantity).to eq 9
+          expect(list_item.notes).to eq 'To make bolts with'
+        end
+
+        it 'updates the aggregate list item' do
+          perform
+          expect(aggregate_list_item.quantity).to eq 9
+          expect(aggregate_list_item.notes).to eq 'To make bolts with'
+        end
+
+        it 'returns a Service::OKResult' do
+          expect(perform).to be_a(Service::OKResult)
+        end
+
+        it 'returns the list item and aggregate list item as the resource' do
+          expect(perform.resource).to eq [aggregate_list_item, list_item.reload]
         end
       end
 
-      context 'when updating the unit_weight' do
-        let(:params) { { unit_weight: 1 } }
-        let(:other_list)  { create(:shopping_list, game: game, aggregate_list: aggregate_list) }
-        let!(:other_item) { create(:shopping_list_item, description: list_item.description, list: other_list, unit_weight: 2) }
+      context 'when there is a matching item on another list' do
+        let!(:list_item)          { create(:shopping_list_item, list: shopping_list, quantity: 4) }
+        let(:other_list)          { create(:shopping_list, game: game, aggregate_list: aggregate_list) }
+        let!(:other_item)         { create(:shopping_list_item, description: list_item.description, list: other_list, quantity: 3) }
+        let(:aggregate_list_item) { aggregate_list.list_items.first }
 
         before do
+          aggregate_list.add_item_from_child_list(list_item)
           aggregate_list.add_item_from_child_list(other_item)
         end
 
-        it 'updates the other item' do
-          perform
-          expect(other_item.reload.unit_weight).to eq 1
+        context 'when the unit weight is not changed' do
+          let(:params) { { quantity: 12 } }
+
+          it 'updates the list item' do
+            perform
+            expect(list_item.reload.quantity).to eq 12
+          end
+
+          it 'updates the aggregate list item' do
+            perform
+            expect(aggregate_list.reload.list_items.first.quantity).to eq 15
+          end
+
+          it 'returns a Service::OKResult' do
+            expect(perform).to be_a(Service::OKResult)
+          end
+
+          it 'sets the resource to the aggregate list item and the regular list item' do
+            expect(perform.resource).to eq [aggregate_list_item, list_item.reload]
+          end
         end
 
-        it 'returns all the items that were updated' do
-          expect(perform.resource.sort).to eq [aggregate_list.list_items.reload.first, list_item, other_item].sort
+        context 'when the unit weight is changed' do
+          let(:params) { { quantity: 10, unit_weight: 2 } }
+
+          it 'updates the list item', :aggregate_failures do
+            perform
+            expect(list_item.reload.quantity).to eq 10
+            expect(list_item.unit_weight).to eq 2
+          end
+
+          it 'updates the aggregate list item', :aggregate_failures do
+            perform
+            expect(aggregate_list_item.quantity).to eq 13
+            expect(aggregate_list_item.unit_weight).to eq 2
+          end
+
+          it 'updates only the unit weight of the other list item', :aggregate_failures do
+            perform
+            expect(other_item.reload.quantity).to eq 3
+            expect(other_item.unit_weight).to eq 2
+          end
+
+          it 'returns a Service::OKResult' do
+            expect(perform).to be_a(Service::OKResult)
+          end
+
+          it 'returns all the list items that were changed' do
+            expect(perform.resource).to eq [aggregate_list_item, other_item.reload, list_item.reload]
+          end
         end
       end
     end
 
-    context 'when the shopping list item is not found' do
-      let(:game)      { create(:game_with_shopping_lists) }
-      let(:list_item) { double("the item that doesn't exist", id: 838) }
-      let(:params)    { { quantity: 3 } }
+    context "when the shopping list item doesn't exist" do
+      let(:list_item) { double(id: 3_459_250) }
+      let(:params)    { { quantity: 4 } }
 
       it 'returns a Service::NotFoundResult' do
         expect(perform).to be_a(Service::NotFoundResult)
       end
 
-      it 'sets the errors to empty' do
-        expect(perform.errors).to eq []
+      it 'leaves the resource and errors empty', :aggregate_failures do
+        expect(perform.resource).to be_blank
+        expect(perform.errors).to be_blank
       end
     end
 
-    context 'when the shopping list item does not belong to the user' do
-      let(:game)      { create(:game_with_shopping_lists) }
+    context "when the shopping list item doesn't belong to the given user" do
       let(:list_item) { create(:shopping_list_item) }
-      let(:params)    { { quantity: 3 } }
+      let(:params)    { { notes: 'Hello world' } }
 
       it 'returns a Service::NotFoundResult' do
         expect(perform).to be_a(Service::NotFoundResult)
       end
 
-      it 'sets the errors to empty' do
-        expect(perform.errors).to eq []
+      it 'leaves the resource and errors empty', :aggregate_failures do
+        expect(perform.resource).to be_blank
+        expect(perform.errors).to be_blank
       end
     end
 
-    context 'when the params are invalid' do
-      let!(:list_item)     { create(:shopping_list_item, list: shopping_list) }
-      let(:game)           { create(:game_with_shopping_lists, user: user) }
-      let(:params)         { { description: 'This is not allowed' } }
-      let(:aggregate_list) { shopping_list.aggregate_list }
-      let(:scope)          { ShoppingListItem.belonging_to_user(user) }
-
-      it 'does not update the aggregate list' do
-        # Put the mocks in here because I don't want this much mocking in the other specs
-        allow(ShoppingListItem).to receive(:belonging_to_user).with(user).and_return(scope)
-        allow(scope).to receive(:find).and_return(list_item)
-        allow(list_item).to receive(:list).and_return(shopping_list)
-        allow(shopping_list).to receive(:aggregate_list).and_return(aggregate_list)
-        allow(aggregate_list).to receive(:update_item_from_child_list)
-        perform
-        expect(aggregate_list).not_to have_received(:update_item_from_child_list).with(list_item.description, 1, nil, nil)
-      end
-
-      it 'does not update the shopping list itself' do
-        t = Time.zone.now + 3.days
-        Timecop.freeze(t) do
-          perform
-          expect(shopping_list.reload.updated_at).not_to be_within(71.hours).of(t)
-        end
-      end
-
-      it 'returns a Service::UnprocessableEntityResult' do
-        expect(perform).to be_a Service::UnprocessableEntityResult
-      end
-
-      it 'returns the errors' do
-        expect(perform.errors).to eq ['Description cannot be updated on an existing list item']
-      end
-    end
-
-    context "when the shopping list item doesn't belong to the authenticated user" do
-      let(:shopping_list) { create(:shopping_list) }
-      let(:list_item)     { create(:shopping_list_item, list: shopping_list) }
-      let(:params)        { { quantity: 4 } }
-
-      it 'returns a Service::NotFoundResult' do
-        expect(perform).to be_a Service::NotFoundResult
-      end
-
-      it 'does not include any errors' do
-        expect(perform.errors).to eq []
-      end
-    end
-
-    context 'when the list item is on an aggregate list' do
-      let(:game)          { create(:game_with_shopping_lists, user: user) }
-      let(:shopping_list) { game.aggregate_shopping_list }
-      let(:list_item)     { create(:shopping_list_item, list: shopping_list) }
-      let(:params)        { { quantity: 4 } }
-
-      it 'does not update the item' do
-        perform
-        expect(list_item.quantity).to eq 1
-      end
+    context 'when the item is on an aggregate list' do
+      let!(:list_item) { create(:shopping_list_item, list: aggregate_list) }
+      let(:params)     { { quantity: 5 } }
 
       it 'returns a Service::MethodNotAllowedResult' do
         expect(perform).to be_a(Service::MethodNotAllowedResult)
@@ -179,14 +153,47 @@ RSpec.describe ShoppingListItemsController::UpdateService do
       end
     end
 
-    context 'when something unexpected goes wrong' do
-      let(:game)          { create(:game_with_shopping_lists, user: user) }
-      let!(:list_item)    { create(:shopping_list_item, list: shopping_list) }
-      let(:shopping_list) { create(:shopping_list, game: game) }
-      let(:params)        { { quantity: 4 } }
+    context 'when the attributes are invalid' do
+      let!(:list_item)          { create(:shopping_list_item, list: shopping_list, quantity: 2) }
+      let(:other_list)          { create(:shopping_list, game: game) }
+      let!(:other_item)         { create(:shopping_list_item, list: other_list, description: list_item.description, quantity: 1) }
+      let(:aggregate_list_item) { aggregate_list.list_items.first }
+      let(:params)              { { quantity: -4, unit_weight: 2 } }
 
       before do
-        allow_any_instance_of(ShoppingListItem).to receive(:save!).and_raise(StandardError, 'Something went horribly wrong')
+        aggregate_list.add_item_from_child_list(list_item)
+        aggregate_list.add_item_from_child_list(other_item)
+      end
+
+      it "doesn't update the aggregate list item", :aggregate_failures do
+        perform
+        expect(aggregate_list_item.quantity).to eq 3
+        expect(aggregate_list_item.unit_weight).to be nil
+      end
+
+      it "doesn't update the other item's unit_weight" do
+        perform
+        expect(other_item.reload.unit_weight).to be nil
+      end
+
+      it 'returns a Service::UnprocessableEntityResult' do
+        expect(perform).to be_a(Service::UnprocessableEntityResult)
+      end
+
+      it 'includes the validation errors' do
+        expect(perform.errors).to eq ['Quantity must be greater than 0']
+      end
+    end
+
+    context 'when there is an unexpected error' do
+      let!(:list_item) { create(:shopping_list_item, list: shopping_list) }
+      let(:params)     { { notes: 'Hello world' } }
+
+      before do
+        aggregate_list.add_item_from_child_list(list_item)
+        allow_any_instance_of(ShoppingList)
+          .to receive(:aggregate)
+                .and_raise(StandardError.new('Something went horribly wrong'))
       end
 
       it 'returns a Service::InternalServerErrorResult' do
@@ -194,7 +201,7 @@ RSpec.describe ShoppingListItemsController::UpdateService do
       end
 
       it 'sets the errors' do
-        expect(perform.errors).to eq(['Something went horribly wrong'])
+        expect(perform.errors).to eq ['Something went horribly wrong']
       end
     end
   end
