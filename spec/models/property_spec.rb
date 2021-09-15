5fbad629b7bd327c138da2ae97fd0109825b9c36
Dana Scheider <dana.scheider@envato.com>
2021-09-16 09:02:43 +1000

    Add more validations for Property model with tests

--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -73,6 +73,7 @@ RSpec.describe Property, type: :model do
           canonical_property: canonical_property,
           name:               canonical_property.name,
           hold:               canonical_property.hold,
+          city:               canonical_property.city,
         )
       end
 
@@ -95,6 +96,81 @@ RSpec.describe Property, type: :model do
         property.hold = canonical_property.hold
         property.validate
         expect(property.errors[:hold]).to eq ['must be unique per game']
+      end
+    end
+
+    describe 'consistency with canonical property' do
+      let(:canonical_property) { CanonicalProperty.find_by(name: 'Severin Manor') }
+
+      it 'must have the same name, hold, and city as the canonical property' do
+        property.canonical_property_id = canonical_property.id
+        property.name                  = canonical_property.name
+        property.hold                  = canonical_property.hold
+        property.city                  = nil
+        property.validate
+        expect(property.errors[:base]).to eq ['property attributes must match attributes of a property that exists in Skyrim']
+      end
+    end
+
+    describe 'arcane enchanter availability' do
+      context 'when an arcane enchanter is not available at the given property' do
+        let(:canonical_property) { CanonicalProperty.find_by(name: 'Breezehome') }
+
+        it 'cannot have an arcane enchanter' do
+          property.canonical_property_id = canonical_property.id
+          property.has_arcane_enchanter  = true
+          property.validate
+          expect(property.errors[:has_arcane_enchanter]).to eq ['cannot be true because this property cannot have an arcane enchanter in Skyrim']
+        end
+      end
+
+      context 'when an arcane enchanter is available at the given property' do
+        let(:canonical_property) { CanonicalProperty.find_by(name: 'Lakeview Manor') }
+
+        it 'can have an arcane enchanter' do
+          property.canonical_property_id = canonical_property.id
+          property.has_arcane_enchanter  = true
+          property.validate
+          expect(property.errors[:has_arcane_enchanter]).to be_blank
+        end
+
+        it "doesn't have to have an arcane enchanter" do
+          property.canonical_property_id = canonical_property.id
+          property.has_arcane_enchanter  = false
+          property.validate
+          expect(property.errors[:has_arcane_enchanter]).to be_blank
+        end
+      end
+    end
+
+    describe 'forge availability' do
+      context 'when a forge is not available at the given property' do
+        let(:canonical_property) { CanonicalProperty.find_by(name: 'Breezehome') }
+
+        it 'cannot have a forge' do
+          property.canonical_property_id = canonical_property.id
+          property.has_forge             = true
+          property.validate
+          expect(property.errors[:has_forge]).to eq ['cannot be true because this property cannot have a forge in Skyrim']
+        end
+      end
+
+      context 'when a forge is available at the given property' do
+        let(:canonical_property) { CanonicalProperty.find_by(name: 'Lakeview Manor') }
+
+        it 'can have a forge' do
+          property.canonical_property_id = canonical_property.id
+          property.has_forge             = true
+          property.validate
+          expect(property.errors[:has_forge]).to be_blank
+        end
+
+        it "doesn't have to have a forge" do
+          property.canonical_property_id = canonical_property.id
+          property.has_forge             = false
+          property.validate
+          expect(property.errors[:has_forge]).to be_blank
+        end
       end
     end
   end
