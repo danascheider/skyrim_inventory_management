37825c64b1804f2d30e8d56f667ed941e81b0787
Dana Scheider <dana.scheider@gmail.com>
2022-04-29 18:36:07 +1000

    Fix Rubocop violations

--- spec/factories/canonical_clothing_items.rb
+++ spec/factories/canonical_clothing_items.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :canonical_clothing_item do
-    sequence(:name) {|n| "Clothing Item #{n}"  }
+    sequence(:name) {|n| "Clothing Item #{n}" }
     unit_weight { 9.9 }
     quest_item { false }
   end
