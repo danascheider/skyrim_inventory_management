6c2ef5ebc52bbfe46d8cd7973b49e882ed2d13f8
Dana Scheider <dana.scheider@gmail.com>
2022-04-29 14:26:42 +1000

    Separate validation specs into the correct contexts

--- spec/models/canonical_armor_spec.rb
+++ spec/models/canonical_armor_spec.rb
@@ -12,13 +12,16 @@ RSpec.describe CanonicalArmor, type: :model do
         expect(armor.errors[:name]).to eq ["can't be blank"]
       end
 
+    describe 'weight' do
       it 'is invalid without a valid weight' do
         armor = described_class.new(name: 'fur armor', body_slot: 'head')
 
         armor.validate
         expect(armor.errors[:weight]).to eq ["can't be blank", 'must be "light armor" or "heavy armor"']
       end
+    end
 
+    describe 'body_slot' do
       it 'is invalid without a valid body slot' do
         armor = described_class.new(name: 'fur armor', weight: 'light armor')
 
