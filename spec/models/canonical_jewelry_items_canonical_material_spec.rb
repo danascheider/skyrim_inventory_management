9dec1faab710908b3759831fd647afa26aea6d5a
Dana Scheider <dana.scheider@gmail.com>
2022-05-04 09:13:43 +1000

    Change 'count' to 'quantity' to fix failing tests

--- spec/models/canonical_jewelry_items_canonical_material_spec.rb
+++ spec/models/canonical_jewelry_items_canonical_material_spec.rb
@@ -9,23 +9,23 @@ RSpec.describe CanonicalJewelryItemsCanonicalMaterial, type: :model do
     let(:jewelry_item) { create(:canonical_jewelry_item) }
     let(:material)     { create(:canonical_material) }
 
-    describe 'count' do
-      it 'is invalid with a count less than 1' do
-        model.count = 0
+    describe 'quantity' do
+      it 'is invalid with a quantity less than 1' do
+        model.quantity = 0
 
         model.validate
-        expect(model.errors[:count]).to include 'must be greater than 0'
+        expect(model.errors[:quantity]).to include 'must be greater than 0'
       end
 
-      it 'is invalid with a non-integer count' do
-        model.count = 1.3
+      it 'is invalid with a non-integer quantity' do
+        model.quantity = 1.3
 
         model.validate
-        expect(model.errors[:count]).to include 'must be an integer'
+        expect(model.errors[:quantity]).to include 'must be an integer'
       end
 
-      it 'is valid with an integer count of at least 1' do
-        model.count = 3
+      it 'is valid with an integer quantity of at least 1' do
+        model.quantity = 3
 
         expect(model).to be_valid
       end
