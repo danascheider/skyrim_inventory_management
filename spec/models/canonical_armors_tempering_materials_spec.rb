f41f17eafcc9a6b951cdb3788d0c0e4ae46facb4
Dana Scheider <dana.scheider@gmail.com>
2022-05-04 07:53:11 +1000

    Add validations for canonical armor and canonical material

--- spec/models/canonical_armors_tempering_materials_spec.rb
+++ spec/models/canonical_armors_tempering_materials_spec.rb
@@ -4,6 +4,14 @@ require 'rails_helper'
 
 RSpec.describe CanonicalArmorsTemperingMaterial, type: :model do
   describe 'validations' do
+    it 'is valid with valid attributes' do
+      material = create(:canonical_material)
+      armor    = create(:canonical_armor)
+      model    = described_class.new(quantity: 3, canonical_material: material, canonical_armor: armor)
+
+      expect(model).to be_valid
+    end
+
     describe 'quantity' do
       it 'is invalid if quantity is 0 or less' do
         material = create(:canonical_material)
@@ -22,13 +30,25 @@ RSpec.describe CanonicalArmorsTemperingMaterial, type: :model do
         model.validate
         expect(model.errors[:quantity]).to include 'must be an integer'
       end
+    end
 
-      it 'is valid with a valid quantity' do
+    describe 'canonical armor' do
+      it 'is invalid without a canonical armor' do
         material = create(:canonical_material)
-        armor    = create(:canonical_armor)
-        model    = described_class.new(quantity: 3, canonical_material: material, canonical_armor: armor)
+        model    = described_class.new(quantity: 10, canonical_material: material)
 
-        expect(model).to be_valid
+        model.validate
+        expect(model.errors[:canonical_armor_id]).to include "can't be blank"
+      end
+    end
+
+    describe 'canonical material' do
+      it 'is invalid without a canonical material' do
+        armor = create(:canonical_armor)
+        model = described_class.new(quantity: 10, canonical_armor: armor)
+
+        model.validate
+        expect(model.errors[:canonical_material_id]).to include "can't be blank"
       end
     end
   end
