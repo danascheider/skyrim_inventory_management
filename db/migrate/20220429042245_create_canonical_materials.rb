e030b29b51c6770c7fe14bfa33bfdfff3e45e29e
Dana Scheider <dana.scheider@gmail.com>
2022-05-03 12:46:56 +1000

    Add item code to canonical materials because these should really exist

--- db/migrate/20220429042245_create_canonical_materials.rb
+++ db/migrate/20220429042245_create_canonical_materials.rb
@@ -3,11 +3,12 @@
 class CreateCanonicalMaterials < ActiveRecord::Migration[6.1]
   def change
     create_table :canonical_materials do |t|
-      t.string :name, null: false, unique: true
+      t.string :name, null: false
+      t.string :item_code, null: false, unique: true
       t.boolean :building_material, default: false
       t.boolean :smithing_material, default: false
       t.decimal :unit_weight, precision: 5, scale: 2
-      t.index :name, unique: true
+      t.index :item_code, unique: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -130,12 +130,13 @@ ActiveRecord::Schema.define(version: 2022_04_30_002324) do
 
   create_table "canonical_materials", force: :cascade do |t|
     t.string "name", null: false
+    t.string "item_code", null: false
     t.boolean "building_material", default: false
     t.boolean "smithing_material", default: false
     t.decimal "unit_weight", precision: 5, scale: 2
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["name"], name: "index_canonical_materials_on_name", unique: true
+    t.index ["item_code"], name: "index_canonical_materials_on_item_code", unique: true
   end
 
   create_table "canonical_properties", force: :cascade do |t|
