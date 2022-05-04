06af2833530564f0fd7531fd56abcd09e77157e8
Dana Scheider <dana.scheider@gmail.com>
2022-05-04 17:49:36 +1000

    Fix error in Rails syntax - pluralise join table name

--- app/models/canonical_ingredient.rb
+++ app/models/canonical_ingredient.rb
@@ -2,7 +2,7 @@
 
 class CanonicalIngredient < ApplicationRecord
   has_many :alchemical_properties_canonical_ingredient, dependent: :destroy
-  has_many :alchemical_properties, through: :alchemical_properties_canonical_ingredient
+  has_many :alchemical_properties, through: :alchemical_properties_canonical_ingredients
 
   validates :name, presence: true
   validates :item_code, presence: true, uniqueness: { message: 'must be unique' }
