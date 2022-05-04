639268de22aeefd056e15da11d2ca05c50d8f1f8
Dana Scheider <dana.scheider@gmail.com>
2022-05-04 19:56:15 +1000

    Fix typo where has_many relation was singular

--- app/models/canonical_ingredient.rb
+++ app/models/canonical_ingredient.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class CanonicalIngredient < ApplicationRecord
-  has_many :alchemical_properties_canonical_ingredient, dependent: :destroy
-  has_many :alchemical_properties, through: :alchemical_properties_canonical_ingredient
+  has_many :alchemical_properties_canonical_ingredients, dependent: :destroy
+  has_many :alchemical_properties, through: :alchemical_properties_canonical_ingredients
 
   validates :name, presence: true
   validates :item_code, presence: true, uniqueness: { message: 'must be unique' }
