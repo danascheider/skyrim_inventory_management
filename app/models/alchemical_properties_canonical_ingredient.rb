2e23327f21a4e02f8510008bc40a0d2f5be7a135
Dana Scheider <dana.scheider@gmail.com>
2022-05-05 10:39:34 +1000

    Add additional validations

--- app/models/alchemical_properties_canonical_ingredient.rb
+++ app/models/alchemical_properties_canonical_ingredient.rb
@@ -5,7 +5,15 @@ class AlchemicalPropertiesCanonicalIngredient < ApplicationRecord
   belongs_to :canonical_ingredient
 
   validates :alchemical_property_id, uniqueness: { scope: :canonical_ingredient_id, message: 'must form a unique combination with canonical ingredient' }
-  validates :priority, uniqueness: { scope: :canonical_ingredient_id, message: 'must be unique per ingredient' }
+  validates :priority,
+            uniqueness:   { scope: :canonical_ingredient_id, message: 'must be unique per ingredient' },
+            numericality: {
+                            greater_than_or_equal_to: 1,
+                            less_than_or_equal_to:    4,
+                            only_integer:             true,
+                          }
+  validates :strength_modifier, allow_blank: true, numericality: { greater_than: 0 }
+  validates :duration_modifier, allow_blank: true, numericality: { greater_than: 0 }
   validate :ensure_max_of_four_per_ingredient, on: :create
 
   MAX_PER_INGREDIENT = 4
--- spec/models/alchemical_properties_canonical_ingredient_spec.rb
+++ spec/models/alchemical_properties_canonical_ingredient_spec.rb
@@ -23,31 +23,72 @@ RSpec.describe AlchemicalPropertiesCanonicalIngredient, type: :model do
     end
 
     describe 'priority' do
-      let(:ingredient) { create(:canonical_ingredient) }
+      describe 'uniqueness' do
+        let(:ingredient) { create(:canonical_ingredient) }
 
-      before do
-        create(
-          :alchemical_properties_canonical_ingredient,
-          priority:             1,
-          canonical_ingredient: ingredient,
-        )
+        before do
+          create(
+            :alchemical_properties_canonical_ingredient,
+            priority:             1,
+            canonical_ingredient: ingredient,
+          )
+        end
+
+        it 'must be unique per ingredient' do
+          model = build(
+                    :alchemical_properties_canonical_ingredient,
+                    priority:             1,
+                    canonical_ingredient: ingredient,
+                  )
+
+          model.validate
+          expect(model.errors[:priority]).to include 'must be unique per ingredient'
+        end
+
+        it "isn't required to be globally unique" do
+          model = build(:alchemical_properties_canonical_ingredient, priority: 1)
+
+          expect(model).to be_valid
+        end
       end
 
-      it 'must be unique per ingredient' do
-        model = build(
-                  :alchemical_properties_canonical_ingredient,
-                  priority:             1,
-                  canonical_ingredient: ingredient,
-                )
+      it "can't be less than 1" do
+        model = build(:alchemical_properties_canonical_ingredient, priority: 0)
 
         model.validate
-        expect(model.errors[:priority]).to include 'must be unique per ingredient'
+        expect(model.errors[:priority]).to include 'must be greater than or equal to 1'
       end
 
-      it "isn't required to be globally unique" do
-        model = build(:alchemical_properties_canonical_ingredient, priority: 1)
+      it "can't be more than 4" do
+        model = build(:alchemical_properties_canonical_ingredient, priority: 5)
 
-        expect(model).to be_valid
+        model.validate
+        expect(model.errors[:priority]).to include 'must be less than or equal to 4'
+      end
+
+      it 'must be an integer' do
+        model = build(:alchemical_properties_canonical_ingredient, priority: 1.5)
+
+        model.validate
+        expect(model.errors[:priority]).to include 'must be an integer'
+      end
+    end
+
+    describe 'strength_modifier' do
+      it 'must be greater than zero' do
+        model = build(:alchemical_properties_canonical_ingredient, strength_modifier: 0)
+
+        model.validate
+        expect(model.errors[:strength_modifier]).to include 'must be greater than 0'
+      end
+    end
+
+    describe 'duration_modifier' do
+      it 'must be greater than zero' do
+        model = build(:alchemical_properties_canonical_ingredient, duration_modifier: 0)
+
+        model.validate
+        expect(model.errors[:duration_modifier]).to include 'must be greater than 0'
       end
     end
   end
