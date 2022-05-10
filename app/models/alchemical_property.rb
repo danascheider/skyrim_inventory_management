700848cdd6ce3a5a3982b75c2982883e531d97f8
Dana Scheider <dana.scheider@gmail.com>
2022-05-11 08:58:47 +1000

    Change name of association that got missed in last PR

--- app/models/alchemical_property.rb
+++ app/models/alchemical_property.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class AlchemicalProperty < ApplicationRecord
-  has_many :alchemical_properties_canonical_ingredients, dependent: :destroy
-  has_many :canonical_ingredients, through: :alchemical_properties_canonical_ingredients
+  has_many :canonical_ingredients_alchemical_properties, dependent: :destroy
+  has_many :canonical_ingredients, through: :canonical_ingredients_alchemical_properties
 
   validates :name, presence: true, uniqueness: { message: 'must be unique' }
   validates :strength_unit, inclusion: { in: %w[point percentage], message: 'must be "point" or "percentage"', allow_blank: true }
