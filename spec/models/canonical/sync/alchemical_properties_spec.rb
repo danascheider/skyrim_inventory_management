88b66dd211b301f3099d0bca619865ae1e2a4c12
Dana Scheider <dana.scheider@gmail.com>
2022-05-11 09:35:14 +1000

    Add and reorganise test cases

--- spec/models/canonical/sync/alchemical_properties_spec.rb
+++ spec/models/canonical/sync/alchemical_properties_spec.rb
@@ -13,123 +13,135 @@ RSpec.describe Canonical::Sync::AlchemicalProperties do
     allow(File).to receive(:read).and_return(json_data)
   end
 
-  context 'when preserve_existing_records is false' do
-    subject(:perform) { described_class.perform(false) }
+  describe '::perform' do
+    subject(:perform) { described_class.perform(preserve_existing_records) }
 
-    let(:syncer) { described_class.new(false) }
+    context 'when preserve_existing_records is false' do
+      let(:preserve_existing_records) { false }
+      let(:syncer)                    { described_class.new(preserve_existing_records) }
 
-    before do
-      allow(described_class).to receive(:new).and_return(syncer)
-    end
+      before do
+        allow(described_class).to receive(:new).and_return(syncer)
+      end
 
-    it 'instantiates itself' do
-      perform
-      expect(described_class).to have_received(:new).with(false)
-    end
-
-    context 'when there are no existing records in the database' do
-      it 'populates the models from the JSON file' do
+      it 'instantiates itself' do
         perform
-        expect(AlchemicalProperty.count).to eq 4
+        expect(described_class).to have_received(:new).with(preserve_existing_records)
+      end
+
+      context 'when there are no existing records in the database' do
+        it 'populates the models from the JSON file' do
+          perform
+          expect(AlchemicalProperty.count).to eq 4
+        end
+      end
+
+      context 'when there are existing records in the database' do
+        let!(:property_in_json)     { create(:alchemical_property, name: 'Cure Disease', strength_unit: 'point') }
+        let!(:property_not_in_json) { create(:alchemical_property, name: 'Restore Health') }
+
+        it 'updates models that were already in the database' do
+          perform
+          expect(property_in_json.reload.strength_unit).to be_nil
+        end
+
+        it "removes models in the database that aren't in the JSON data" do
+          perform
+          expect(AlchemicalProperty.find_by(name: 'Restore Health')).to be_nil
+        end
+
+        it 'adds new models to the database', :aggregate_failures do
+          perform
+          expect(AlchemicalProperty.find_by(name: 'Damage Health')).to be_present
+          expect(AlchemicalProperty.find_by(name: 'Damage Magicka')).to be_present
+          expect(AlchemicalProperty.find_by(name: 'Damage Magicka Regen')).to be_present
+        end
       end
     end
 
-    context 'when there are existing records in the database' do
-      let!(:property_in_json)     { create(:alchemical_property, name: 'Cure Disease', strength_unit: 'point') }
-      let!(:property_not_in_json) { create(:alchemical_property, name: 'Restore Health') }
+    context 'when preserve_existing_records is true' do
+      let(:preserve_existing_records) { true }
+      let(:syncer)                    { described_class.new(preserve_existing_records) }
+      let!(:property_in_json)         { create(:alchemical_property, name: 'Cure Disease', strength_unit: 'percentage') }
+      let!(:property_not_in_json)     { create(:alchemical_property, name: 'Restore Health') }
 
-      it 'updates models that were already in the database' do
+      before do
+        allow(Canonical::Sync::Enchantments).to receive(:new)
+      end
+
+      it 'instantiates itself' do
+        perform
+        expect(Canonical::Sync::Enchantments).to have_received(:new).with(preserve_existing_records)
+      end
+
+      it 'updates properties found in the JSON data' do
         perform
         expect(property_in_json.reload.strength_unit).to be_nil
       end
 
-      it "removes models in the database that aren't in the JSON data" do
-        perform
-        expect(AlchemicalProperty.find_by(name: 'Restore Health')).to be_nil
-      end
-
-      it 'adds new models to the database', :aggregate_failures do
+      it 'adds models not already in the database', :aggregate_failures do
         perform
         expect(AlchemicalProperty.find_by(name: 'Damage Health')).to be_present
         expect(AlchemicalProperty.find_by(name: 'Damage Magicka')).to be_present
         expect(AlchemicalProperty.find_by(name: 'Damage Magicka Regen')).to be_present
       end
-    end
-  end
 
-  context 'when preserve_existing_records is true' do
-    subject(:perform) { described_class.perform(true) }
-
-    let!(:property_in_json)     { create(:alchemical_property, name: 'Cure Disease', strength_unit: 'percentage') }
-    let!(:property_not_in_json) { create(:alchemical_property, name: 'Restore Health') }
-
-    it 'updates properties found in the JSON data' do
-      perform
-      expect(property_in_json.reload.strength_unit).to be_nil
-    end
-
-    it 'adds models not already in the database', :aggregate_failures do
-      perform
-      expect(AlchemicalProperty.find_by(name: 'Damage Health')).to be_present
-      expect(AlchemicalProperty.find_by(name: 'Damage Magicka')).to be_present
-      expect(AlchemicalProperty.find_by(name: 'Damage Magicka Regen')).to be_present
-    end
-
-    it "doesn't destroy models that aren't in the JSON data" do
-      perform
-      expect(property_not_in_json.reload).to be_present
-    end
-  end
-
-  describe 'error logging' do
-    subject(:perform) { described_class.perform(false) }
-
-    context 'when an ActiveRecord::RecordInvalid error is raised' do
-      let(:errored_model) do
-        instance_double AlchemicalProperty,
-                        errors: errors,
-                        class:  class_double(AlchemicalProperty, i18n_scope: :activerecord)
-      end
-
-      let(:errors) { double('errors', full_messages: ["Name can't be blank"]) }
-
-      before do
-        allow_any_instance_of(AlchemicalProperty)
-          .to receive(:save!)
-                .and_raise(ActiveRecord::RecordInvalid, errored_model)
-        allow(Rails.logger).to receive(:error)
-      end
-
-      it 'logs and reraises the error', :aggregate_failures do
-        expect { perform }
-          .to raise_error(ActiveRecord::RecordInvalid)
-        expect(Rails.logger).to have_received(:error).with("Error saving alchemical property \"Cure Disease\": Validation failed: Name can't be blank")
+      it "doesn't destroy models that aren't in the JSON data" do
+        perform
+        expect(property_not_in_json.reload).to be_present
       end
     end
 
-    context 'when another error is raised pertaining to a specific model' do
-      before do
-        allow(AlchemicalProperty).to receive(:find_or_initialize_by).and_raise(StandardError, 'foobar')
-        allow(Rails.logger).to receive(:error)
+    describe 'error logging' do
+      let(:preserve_existing_records) { false }
+
+      context 'when an ActiveRecord::RecordInvalid error is raised' do
+        let(:errored_model) do
+          instance_double AlchemicalProperty,
+                          errors: errors,
+                          class:  class_double(AlchemicalProperty, i18n_scope: :activerecord)
+        end
+
+        let(:errors) { double('errors', full_messages: ["Name can't be blank"]) }
+
+        before do
+          allow_any_instance_of(AlchemicalProperty)
+            .to receive(:save!)
+                  .and_raise(ActiveRecord::RecordInvalid, errored_model)
+          allow(Rails.logger).to receive(:error)
+        end
+
+        it 'logs and reraises the error', :aggregate_failures do
+          expect { perform }
+            .to raise_error(ActiveRecord::RecordInvalid)
+          expect(Rails.logger).to have_received(:error).with("Error saving alchemical property \"Cure Disease\": Validation failed: Name can't be blank")
+        end
       end
 
-      it 'logs and reraises the error', :aggregate_failures do
-        expect { perform }
-          .to raise_error(StandardError)
-        expect(Rails.logger).to have_received(:error).with('Unexpected error StandardError saving alchemical property "Cure Disease": foobar')
-      end
-    end
+      context 'when another error is raised pertaining to a specific model' do
+        before do
+          allow(AlchemicalProperty).to receive(:find_or_initialize_by).and_raise(StandardError, 'foobar')
+          allow(Rails.logger).to receive(:error)
+        end
 
-    context 'when an error is raised not pertaining to a specific model' do
-      before do
-        allow(AlchemicalProperty).to receive(:where).and_raise(StandardError, 'foobar')
-        allow(Rails.logger).to receive(:error)
+        it 'logs and reraises the error', :aggregate_failures do
+          expect { perform }
+            .to raise_error(StandardError)
+          expect(Rails.logger).to have_received(:error).with('Unexpected error StandardError saving alchemical property "Cure Disease": foobar')
+        end
       end
 
-      it 'logs and reraises the error', :aggregate_failures do
-        expect { perform }
-          .to raise_error(StandardError)
-        expect(Rails.logger).to have_received(:error).with('Unexpected error StandardError while syncing alchemical properties: foobar')
+      context 'when an error is raised not pertaining to a specific model' do
+        before do
+          allow(AlchemicalProperty).to receive(:where).and_raise(StandardError, 'foobar')
+          allow(Rails.logger).to receive(:error)
+        end
+
+        it 'logs and reraises the error', :aggregate_failures do
+          expect { perform }
+            .to raise_error(StandardError)
+          expect(Rails.logger).to have_received(:error).with('Unexpected error StandardError while syncing alchemical properties: foobar')
+        end
       end
     end
   end
