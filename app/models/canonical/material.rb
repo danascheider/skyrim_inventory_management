3ed4424d191b0c8d6646839f2ed9e91b69c76919
Dana Scheider <dana.scheider@gmail.com>
2022-05-10 21:24:21 +1000

    Fix small issues and update Rake task to use new class and association names

--- app/models/canonical/material.rb
+++ app/models/canonical/material.rb
@@ -19,14 +19,14 @@ module Canonical
     has_many :canonical_weapons_smithing_materials,
              dependent:  :destroy,
              class_name: 'Canonical::WeaponsSmithingMaterial',
-             inverse_of: :canonical_material
-    has_many :smithable_weapons, through: :canonical_weapons_smithing_materials, source: :canonical_weapon
+             inverse_of: :material
+    has_many :smithable_weapons, through: :canonical_weapons_smithing_materials, source: :weapon
 
     has_many :canonical_weapons_tempering_materials,
              dependent:  :destroy,
              class_name: 'Canonical::WeaponsTemperingMaterial',
-             inverse_of: :canonical_material
-    has_many :temperable_weapons, through: :canonical_weapons_tempering_materials, source: :canonical_weapon
+             inverse_of: :material
+    has_many :temperable_weapons, through: :canonical_weapons_tempering_materials, source: :weapon
 
     has_many :canonical_jewelry_items_materials,
              dependent:  :destroy,
