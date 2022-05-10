d44bf73d28919605c2d0fbfd1eb148a7b872e16a
Dana Scheider <dana.scheider@gmail.com>
2022-05-11 08:57:18 +1000

    Revert "Rename associations that got missed in previous PR" because it wasn't supposed to be committed directly to the feature branch
    This reverts commit 5210ed43e9f5ad7dd13484cc3edcf01baec19f45.

--- app/models/alchemical_property.rb
+++ app/models/alchemical_property.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 class AlchemicalProperty < ApplicationRecord
-  has_many :canonical_ingredients_alchemical_properties,
-           dependent:  :destroy,
-           class_name: 'Canonical::IngredientsAlchemicalProperty'
-  has_many :canonical_ingredients, through: :canonical_ingredients_alchemical_properties
+  has_many :alchemical_properties_canonical_ingredients, dependent: :destroy
+  has_many :canonical_ingredients, through: :alchemical_properties_canonical_ingredients
 
   validates :name, presence: true, uniqueness: { message: 'must be unique' }
   validates :strength_unit, inclusion: { in: %w[point percentage], message: 'must be "point" or "percentage"', allow_blank: true }
