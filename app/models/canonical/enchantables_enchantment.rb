53c6f28250db10dc9eac78d3196fadccd8e087aa
Dana Scheider <dana.scheider@gmail.com>
2022-05-15 10:24:43 +1000

    Use symbol array instead of string array

--- app/models/canonical/enchantables_enchantment.rb
+++ app/models/canonical/enchantables_enchantment.rb
@@ -9,6 +9,6 @@ module Canonical
     belongs_to :enchantable, polymorphic: true
     belongs_to :enchantment
 
-    validates :enchantment_id, uniqueness: { scope: %w[enchantable_id enchantable_type], message: 'must form a unique combination with enchantable item' }
+    validates :enchantment_id, uniqueness: { scope: %i[enchantable_id enchantable_type], message: 'must form a unique combination with enchantable item' }
   end
 end
