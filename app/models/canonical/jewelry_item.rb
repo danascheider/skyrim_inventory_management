ca4e109aa801de489ba46d1db7fcbd0cd36a66e3
Dana Scheider <dana.scheider@gmail.com>
2022-05-29 14:39:08 +1000

    Add validations to model for boolean fields, update tests and fixutres

--- app/models/canonical/jewelry_item.rb
+++ app/models/canonical/jewelry_item.rb
@@ -4,6 +4,9 @@ module Canonical
   class JewelryItem < ApplicationRecord
     self.table_name = 'canonical_jewelry_items'
 
+    BOOLEAN_VALUES             = [true, false].freeze
+    BOOLEAN_VALIDATION_MESSAGE = 'must be true or false'
+
     has_many :canonical_enchantables_enchantments,
              dependent:  :destroy,
              class_name: 'Canonical::EnchantablesEnchantment',
@@ -27,9 +30,21 @@ module Canonical
               presence:  true,
               inclusion: { in: %w[ring circlet amulet], message: 'must be "ring", "circlet", or "amulet"' }
     validates :unit_weight, presence: true, numericality: { greater_than_or_equal_to: 0 }
+    validates :purchasable, inclusion: { in: BOOLEAN_VALUES, message: BOOLEAN_VALIDATION_MESSAGE }
+    validates :unique_item, inclusion: { in: BOOLEAN_VALUES, message: BOOLEAN_VALIDATION_MESSAGE }
+    validates :rare_item, inclusion: { in: BOOLEAN_VALUES, message: BOOLEAN_VALIDATION_MESSAGE }
+    validates :quest_item, inclusion: { in: BOOLEAN_VALUES, message: BOOLEAN_VALIDATION_MESSAGE }
+
+    validate :validate_unique_item_also_rare, if: -> { unique_item == true }
 
     def self.unique_identifier
       :item_code
+    end
+
+    private
+
+    def validate_unique_item_also_rare
+      errors.add(:rare_item, 'must be true if item is unique') unless rare_item == true
     end
   end
 end
--- spec/models/canonical/jewelry_item_spec.rb
+++ spec/models/canonical/jewelry_item_spec.rb
@@ -5,34 +5,28 @@ require 'rails_helper'
 RSpec.describe Canonical::JewelryItem, type: :model do
   describe 'validations' do
     describe 'name' do
-      it 'is invalid with no name' do
-        item = described_class.new(item_code: 'xxx', jewelry_type: 'amulet', unit_weight: 1.0)
+      it "can't be blank" do
+        model = build(:canonical_jewelry_item, name: nil)
 
-        item.validate
-        expect(item.errors[:name]).to include "can't be blank"
-      end
-
-      it 'is valid with a name' do
-        item = build(:canonical_jewelry_item, name: 'foo')
-
-        expect(item).to be_valid
+        model.validate
+        expect(model.errors[:name]).to include "can't be blank"
       end
     end
 
     describe 'item_code' do
-      it 'is invalid without an item code' do
-        item = described_class.new(name: 'foo', jewelry_type: 'amulet', unit_weight: 4.2)
+      it "can't be blank" do
+        model = build(:canonical_jewelry_item, item_code: nil)
 
-        item.validate
-        expect(item.errors[:item_code]).to include "can't be blank"
+        model.validate
+        expect(model.errors[:item_code]).to include "can't be blank"
       end
 
-      it 'is invalid with a non-unique item code' do
+      it 'must be unique' do
         create(:canonical_jewelry_item, item_code: 'xxx')
-        item = build(:canonical_jewelry_item, item_code: 'xxx')
+        model = build(:canonical_jewelry_item, item_code: 'xxx')
 
-        item.validate
-        expect(item.errors[:item_code]).to include 'must be unique'
+        model.validate
+        expect(model.errors[:item_code]).to include 'must be unique'
       end
 
       it 'is valid with a unique item code' do
@@ -44,46 +38,83 @@ RSpec.describe Canonical::JewelryItem, type: :model do
 
     describe 'jewelry_type' do
       it 'is invalid without a jewelry_type' do
-        item = described_class.new(name: 'foo', item_code: 'xxx', unit_weight: 1.0)
+        model = build(:canonical_jewelry_item, jewelry_type: nil)
 
-        item.validate
-        expect(item.errors[:jewelry_type]).to include "can't be blank"
+        model.validate
+        expect(model.errors[:jewelry_type]).to include "can't be blank"
       end
 
       it 'is invalid with an invalid jewelry_type' do
-        item = build(:canonical_jewelry_item, jewelry_type: 'bar')
+        model = build(:canonical_jewelry_item, jewelry_type: 'bar')
 
-        item.validate
-        expect(item.errors[:jewelry_type]).to include 'must be "ring", "circlet", or "amulet"'
-      end
-
-      it 'is valid with a valid jewelry_type' do
-        item = build(:canonical_jewelry_item, jewelry_type: 'circlet')
-
-        expect(item).to be_valid
+        model.validate
+        expect(model.errors[:jewelry_type]).to include 'must be "ring", "circlet", or "amulet"'
       end
     end
 
     describe 'unit_weight' do
       it 'is invalid without a unit weight' do
-        item = described_class.new(name: 'foo', jewelry_type: 'ring')
+        model = build(:canonical_jewelry_item, unit_weight: nil)
 
-        item.validate
-        expect(item.errors[:unit_weight]).to include "can't be blank"
+        model.validate
+        expect(model.errors[:unit_weight]).to include "can't be blank"
       end
 
       it 'is invalid with a non-numeric unit weight' do
-        item = build(:canonical_jewelry_item, unit_weight: 'bar')
+        model = build(:canonical_jewelry_item, unit_weight: 'bar')
 
-        item.validate
-        expect(item.errors[:unit_weight]).to include 'is not a number'
+        model.validate
+        expect(model.errors[:unit_weight]).to include 'is not a number'
       end
 
       it 'is invalid with a negative unit weight' do
-        item = build(:canonical_jewelry_item, unit_weight: -4.3)
+        model = build(:canonical_jewelry_item, unit_weight: -4.3)
 
-        item.validate
-        expect(item.errors[:unit_weight]).to include 'must be greater than or equal to 0'
+        model.validate
+        expect(model.errors[:unit_weight]).to include 'must be greater than or equal to 0'
+      end
+    end
+
+    describe 'purchasable' do
+      it 'must be true or false' do
+        model = build(:canonical_jewelry_item, purchasable: nil)
+
+        model.validate
+        expect(model.errors[:purchasable]).to include 'must be true or false'
+      end
+    end
+
+    describe 'unique_item' do
+      it 'must be true or false' do
+        model = build(:canonical_jewelry_item, unique_item: nil)
+
+        model.validate
+        expect(model.errors[:unique_item]).to include 'must be true or false'
+      end
+    end
+
+    describe 'rare_item' do
+      it 'must be true or false' do
+        model = build(:canonical_jewelry_item, rare_item: nil)
+
+        model.validate
+        expect(model.errors[:rare_item]).to include 'must be true or false'
+      end
+
+      it 'must be true if the item is unique' do
+        model = build(:canonical_jewelry_item, unique_item: true, rare_item: false)
+
+        model.validate
+        expect(model.errors[:rare_item]).to include 'must be true if item is unique'
+      end
+    end
+
+    describe 'quest_item' do
+      it 'must be true or false' do
+        model = build(:canonical_jewelry_item, quest_item: nil)
+
+        model.validate
+        expect(model.errors[:quest_item]).to include 'must be true or false'
       end
     end
   end
