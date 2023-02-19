128a721790f766a71cc7745a0ff814d7c496733c
Dana Scheider <dana.scheider@gmail.com>
2023-02-20 06:59:50 +1100

    Remove Google OAuth client ID config option since for now we aren't using that

--- config/configatron/defaults.rb
+++ config/configatron/defaults.rb
@@ -1,4 +1,3 @@
 # frozen_string_literal: true
 
-configatron.client_origin          = ENV['CLIENT_ORIGIN'] || 'http://localhost:5173'
-configatron.google_oauth_client_id = ENV['GOOGLE_OAUTH_CLIENT_ID']
+configatron.client_origin = ENV['CLIENT_ORIGIN'] || 'http://localhost:5173'
