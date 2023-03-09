5e4c5c5bdf038c3bd85e4a846e0dbea78db633ea
Dana Scheider <dana.scheider@gmail.com>
2023-03-09 17:27:52 +1100

    Don't automatically assign models belonging to users to the first user - that was temporary when we pulled out auth

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,17 +3,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  before_create :assign_user
-
   def error_array
     errors.map {|error| "#{error.attribute.capitalize} #{error.message}" }
-  end
-
-  private
-
-  def assign_user
-    return unless respond_to?(:user=)
-
-    self.user = User.first
   end
 end
