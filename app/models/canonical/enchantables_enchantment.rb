b82a777c456c28ba673feabff46df39cba935d6a
Dana Scheider <dana.scheider@gmail.com>
2023-05-20 08:55:49 +1000

    Remove extra frozen_string_literal comment

--- app/models/canonical/enchantables_enchantment.rb
+++ app/models/canonical/enchantables_enchantment.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-# frozen_string_literal
-
 module Canonical
   class EnchantablesEnchantment < ApplicationRecord
     self.table_name = 'canonical_enchantables_enchantments'
