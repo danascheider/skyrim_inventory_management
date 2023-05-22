872e713729af78b0f2a954c85a359406e696f9eb
Dana Scheider <dana.scheider@gmail.com>
2023-05-23 08:25:15 +1000

    Further flesh out ClothingItem specs

--- app/models/clothing_item.rb
+++ app/models/clothing_item.rb
@@ -18,4 +18,26 @@ class ClothingItem < ApplicationRecord
 
   validates :name, presence: true
   validates :unit_weight, numericality: { greater_than_or_equal_to: 0, allow_nil: true }
+
+  before_validation :set_canonical_clothing_item
+
+  def canonical_clothing_items
+    return Array.wrap(canonical_clothing_item) if canonical_clothing_item
+
+    attrs_to_match = { unit_weight:, magical_effects: }.compact
+
+    canonicals = Canonical::ClothingItem.where('name ILIKE ?', name)
+    attrs_to_match.any? ? canonicals.where(**attrs_to_match) : canonicals
+  end
+
+  private
+
+  def set_canonical_clothing_item
+    return unless canonical_clothing_items.count == 1
+
+    self.canonical_clothing_item ||= canonical_clothing_items.first
+    self.name = canonical_clothing_item.name # in case casing differs
+    self.unit_weight = canonical_clothing_item.unit_weight
+    self.magical_effects = canonical_clothing_item.magical_effects
+  end
 end
--- spec/models/clothing_item_spec.rb
+++ spec/models/clothing_item_spec.rb
@@ -20,4 +20,136 @@ RSpec.describe ClothingItem, type: :model do
 
     it 'validates against canonical models'
   end
+
+  describe '::before_validation' do
+    context 'when there is a single matching canonical model' do
+      let!(:matching_canonical) do
+        create(
+          :canonical_clothing_item,
+          :with_enchantments,
+          name: 'Fine Clothes',
+          unit_weight: 1,
+          magical_effects: 'Something',
+        )
+      end
+
+      let(:item) do
+        build(
+          :clothing_item,
+          name: 'Fine clothes',
+          unit_weight: 1,
+        )
+      end
+
+      before do
+        create(:canonical_clothing_item, name: 'Fine Clothes', unit_weight: 2)
+      end
+
+      it 'assigns the canonical clothing item' do
+        item.validate
+        expect(item.canonical_clothing_item).to eq matching_canonical
+      end
+
+      it 'sets the attributes', :aggregate_failures do
+        item.validate
+        expect(item.name).to eq 'Fine Clothes'
+        expect(item.magical_effects).to eq 'Something'
+      end
+    end
+
+    context 'when there are multiple matching canonical models' do
+      let!(:matching_canonicals) do
+        create_list(
+          :canonical_clothing_item,
+          2,
+          :with_enchantments,
+          name: 'Fine Clothes',
+          unit_weight: 1,
+        )
+      end
+
+      let(:item) { build(:clothing_item, name: 'Fine clothes') }
+
+      it "doesn't set the corresponding canonical clothing item" do
+        item.validate
+        expect(item.canonical_clothing_item).to be_nil
+      end
+
+      it "doesn't set other attributes", :aggregate_failures do
+        item.validate
+        expect(item.name).to eq 'Fine clothes'
+        expect(item.unit_weight).to be_nil
+        expect(item.magical_effects).to be_nil
+      end
+    end
+
+    context 'when there are no matching canonical models' do
+      let(:item) { build(:clothing_item) }
+
+      it 'is invalid' do
+        pending
+        item.validate
+        expect(item.errors[:base]).to include "doesn't match a clothing item that exists in Skyrim"
+      end
+    end
+  end
+
+  describe '#canonical_clothing_items' do
+    subject(:canonical_clothing_items) { item.canonical_clothing_items }
+
+    context 'when the item has an association defined' do
+      let(:item) do
+        create(
+          :clothing_item,
+          canonical_clothing_item:,
+          name: 'Fine Clothes',
+          unit_weight: 1,
+          magical_effects: nil,
+        )
+      end
+
+      let(:canonical_clothing_item) do
+        create(
+          :canonical_clothing_item,
+          name: 'Fine Clothes',
+          unit_weight: 1,
+          magical_effects: nil,
+        )
+      end
+
+      it 'returns the associated model in an array' do
+        expect(canonical_clothing_items).to eq [canonical_clothing_item]
+      end
+    end
+
+    context 'when the item does not have an association defined' do
+      before do
+        create(:canonical_clothing_item, name: 'Something Else')
+      end
+
+      context 'when only the name has to match' do
+        let!(:matching_canonicals) { create_list(:canonical_clothing_item, 3, name: item.name, unit_weight: 2.5) }
+
+        let(:item) { build(:clothing_item, unit_weight: nil) }
+
+        it 'returns all matching items' do
+          expect(canonical_clothing_items).to eq matching_canonicals
+        end
+      end
+
+      context 'when multiple attributes have to match' do
+        let!(:matching_canonicals) { create_list(:canonical_clothing_item, 3, name: item.name, unit_weight: 2.5) }
+
+        let(:item) { build(:clothing_item, unit_weight: 2.5) }
+
+        before do
+          create(:canonical_clothing_item, name: item.name, unit_weight: 1)
+        end
+
+        it 'returns only the items for which all values match' do
+          expect(canonical_clothing_items).to eq matching_canonicals
+        end
+      end
+    end
+  end
 end
