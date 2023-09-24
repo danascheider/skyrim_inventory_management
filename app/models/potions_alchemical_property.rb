a4b52fc13bab268b41f15f8db0d5042a8d90a1c9
Dana Scheider <dana.scheider@gmail.com>
2023-09-25 06:25:26 +1000

    Remove extraneous association with a canonical model to reduce database queries

--- app/models/potions_alchemical_property.rb
+++ app/models/potions_alchemical_property.rb
@@ -21,39 +21,4 @@ class PotionsAlchemicalProperty < ApplicationRecord
               only_integer: true,
               allow_nil: true,
             }
-
-  before_validation :set_attributes_from_canonical, if: -> { canonical_model.present? }
-
-  delegate :canonical_potions, :canonical_potion, to: :potion
-
-  def canonical_models
-    return Canonical::PotionsAlchemicalProperty.where(**attributes_to_match) if attributes_to_match.any?
-
-    Canonical::PotionsAlchemicalProperty.none
-  end
-
-  def canonical_model
-    @canonical_model ||= begin
-      models = canonical_models
-      models.first if models.count == 1
-    end
-  end
-
-  private
-
-  def set_attributes_from_canonical
-    return if canonical_model.nil?
-
-    self.strength = canonical_model.strength
-    self.duration = canonical_model.duration
-  end
-
-  def attributes_to_match
-    {
-      potion_id: canonical_potions.ids.presence,
-      alchemical_property_id:,
-      strength:,
-      duration:,
-    }.compact
-  end
 end
