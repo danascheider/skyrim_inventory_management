1cc6dbce4c08bcd8749ef42d2860b621dccca348
Dana Scheider <dana.scheider@gmail.com>
2023-10-30 08:26:12 +1100

    Prevent canonical clothing items from having multiple clothing item associations for the same game (if unique)

--- app/validators/clothing_item_validator.rb
+++ app/validators/clothing_item_validator.rb
@@ -3,6 +3,7 @@
 class ClothingItemValidator < ActiveModel::Validator
   NO_CANONICAL_MATCHES = "doesn't match a clothing item that exists in Skyrim"
   DOES_NOT_MATCH = 'does not match value on canonical model'
+  DUPLICATE_MATCH = 'is a duplicate of a unique in-game item'
 
   def validate(record)
     @record = record
@@ -22,7 +23,23 @@ class ClothingItemValidator < ActiveModel::Validator
   def validate_against_canonical
     canonical = record.canonical_clothing_item
 
+    validate_unique_canonical
+
     record.errors.add(:unit_weight, DOES_NOT_MATCH) unless record.unit_weight == canonical.unit_weight
     record.errors.add(:magical_effects, DOES_NOT_MATCH) unless record.magical_effects == canonical.magical_effects
+  end
+
+  def validate_unique_canonical
+    return unless record.canonical_clothing_item&.unique_item
+
+    items = record
+              .canonical_clothing_item
+              .clothing_items
+              .where(game_id: record.game_id)
+
+    return if items.count < 1
+    return if items.count == 1 && items.first == record
+
+    record.errors.add(:base, DUPLICATE_MATCH)
   end
 end
