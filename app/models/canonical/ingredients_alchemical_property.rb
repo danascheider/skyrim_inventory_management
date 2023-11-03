aad6dbc16e5b83134b50f8214a3a4e3ee306ed3a
Dana Scheider <dana.scheider@gmail.com>
2023-11-04 07:58:33 +1100

    Format code better and rename method to avoid referencing specific constant value

--- app/models/canonical/ingredients_alchemical_property.rb
+++ app/models/canonical/ingredients_alchemical_property.rb
@@ -7,7 +7,11 @@ module Canonical
     belongs_to :alchemical_property
     belongs_to :ingredient, class_name: 'Canonical::Ingredient'
 
-    validates :alchemical_property_id, uniqueness: { scope: :ingredient_id, message: 'must form a unique combination with canonical ingredient' }
+    validates :alchemical_property_id,
+              uniqueness: {
+                scope: :ingredient_id,
+                message: 'must form a unique combination with canonical ingredient',
+              }
     validates :priority,
               allow_blank: true,
               uniqueness: { scope: :ingredient_id, message: 'must be unique per ingredient' },
@@ -18,13 +22,13 @@ module Canonical
               }
     validates :strength_modifier, allow_blank: true, numericality: { greater_than: 0 }
     validates :duration_modifier, allow_blank: true, numericality: { greater_than: 0 }
-    validate :ensure_max_of_four_per_ingredient
+    validate :ensure_max_per_ingredient
 
     MAX_PER_INGREDIENT = 4
 
     private
 
-    def ensure_max_of_four_per_ingredient
+    def ensure_max_per_ingredient
       return if ingredient.alchemical_properties.length < MAX_PER_INGREDIENT
       return if persisted? &&
         !ingredient_id_changed? &&
