9ec2c6ba48466d252f5c14e466434e99c96346e8
Dana Scheider <dana.scheider@gmail.com>
2023-11-06 09:32:32 +1100

    Remove jewelry_type column from jewelry_items table since it can be delegated to a canonical model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_02_193432) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_05_222944) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -415,7 +415,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_02_193432) do
     t.bigint "canonical_jewelry_item_id"
     t.string "name", null: false
     t.decimal "unit_weight", precision: 5, scale: 2
-    t.string "jewelry_type"
     t.string "magical_effects"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
