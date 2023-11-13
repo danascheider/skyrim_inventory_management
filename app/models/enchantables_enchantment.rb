b6821f08e5684d8e2fbdfc5551e79bcea43cc89c
Dana Scheider <dana.scheider@gmail.com>
2023-11-14 06:35:08 +1100

    Add validations, scope, other behaviour around added_automatically attribute

--- app/models/enchantables_enchantment.rb
+++ app/models/enchantables_enchantment.rb
@@ -4,12 +4,26 @@ class EnchantablesEnchantment < ApplicationRecord
   belongs_to :enchantable, polymorphic: true
   belongs_to :enchantment
 
-  validates :enchantment_id, uniqueness: { scope: %i[enchantable_id enchantable_type], message: 'must form a unique combination with enchantable item' }
+  validates :enchantment_id,
+            uniqueness: {
+              scope: %i[enchantable_id enchantable_type],
+              message: 'must form a unique combination with enchantable item',
+            }
+
+  validates :added_automatically,
+            inclusion: {
+              in: [true, false],
+              message: 'must be true or false',
+            }
+
+  before_validation :set_added_automatically
 
   after_validation :validate_against_canonical,
                    if: :should_validate_against_canonical?
 
   after_save :save_associated!, unless: :canonical_enchantable?
+
+  scope :added_manually, -> { where(added_automatically: false) }
 
   private
 
@@ -29,6 +43,11 @@ class EnchantablesEnchantment < ApplicationRecord
 
   def should_validate_against_canonical?
     errors.none? && !canonical_enchantable?
+  end
+
+  def set_added_automatically
+    self.added_automatically = true if canonical_enchantable?
+    self.added_automatically = false if added_automatically.nil?
   end
 
   def canonical_enchantable?
