d8b88e4e97a69dc44b2f58cebbe71acb6f75ea51
Dana Scheider <dana.scheider@gmail.com>
2023-11-25 12:18:34 +1100

    Extract abstract EnchantableInGameItem model from which armor, clothing items, jewelry items, and weapons can all inherit

--- app/models/armor.rb
+++ app/models/armor.rb
@@ -1,20 +1,10 @@
 # frozen_string_literal: true
 
-class Armor < ApplicationRecord
-  belongs_to :game
-
+class Armor < EnchantableInGameItem
   belongs_to :canonical_armor,
              optional: true,
              inverse_of: :armors,
              class_name: 'Canonical::Armor'
-
-  has_many :enchantables_enchantments,
-           dependent: :destroy,
-           as: :enchantable
-  has_many :enchantments,
-           -> { select 'enchantments.*, enchantables_enchantments.strength as strength' },
-           through: :enchantables_enchantments,
-           source: :enchantment
 
   validates :name, presence: true
 
@@ -33,41 +23,8 @@ class Armor < ApplicationRecord
 
   validates_with ArmorValidator
 
-  before_validation :set_canonical_armor
-
-  after_create :set_enchantments, if: -> { canonical_armor.present? }
-
   def canonical_model
     canonical_armor
-  end
-
-  def canonical_models
-    return Canonical::Armor.where(id: canonical_armor_id) if canonical_model_matches?
-
-    query = 'name ILIKE :name'
-    query += ' AND magical_effects ILIKE :magical_effects' if magical_effects.present?
-
-    canonicals = Canonical::Armor.where(query, name:, magical_effects:)
-    canonicals = canonicals.where(**attributes_to_match) if attributes_to_match.any?
-
-    return canonicals if enchantments.none?
-
-    enchantables_enchantments.added_manually.each do |join_model|
-      canonicals = if join_model.strength.present?
-                     canonicals.left_outer_joins(:enchantables_enchantments).where(
-                       '(enchantables_enchantments.enchantment_id = :enchantment_id AND enchantables_enchantments.strength = :strength) OR canonical_armors.enchantable = true',
-                       enchantment_id: join_model.enchantment_id,
-                       strength: join_model.strength,
-                     )
-                   else
-                     canonicals.left_outer_joins(:enchantables_enchantments).where(
-                       '(enchantables_enchantments.enchantment_id = :enchantment_id AND enchantables_enchantments.strength IS NULL) OR canonical_armors.enchantable = true',
-                       enchantment_id: join_model.enchantment_id,
-                     )
-                   end
-    end
-
-    Canonical::Armor.where(id: canonicals.ids)
   end
 
   def crafting_materials
@@ -80,39 +37,34 @@ class Armor < ApplicationRecord
 
   private
 
-  def set_canonical_armor
-    canonicals = canonical_models
-
-    unless canonicals.count == 1
-      clear_canonical_armor
-      return
-    end
-
-    self.canonical_armor = canonicals.first
-    self.name = canonical_armor.name # in case casing differs
-    self.magical_effects = canonical_armor.magical_effects
-    self.unit_weight = canonical_armor.unit_weight
-    self.weight = canonical_armor.weight
-
-    set_enchantments if persisted? && canonical_armor_id_changed?
+  def canonical_class
+    Canonical::Armor
   end
 
-  def set_enchantments
-    return if canonical_armor.enchantments.empty?
-
-    remove_automatically_added_enchantments!
-
-    canonical_armor.enchantables_enchantments.each do |model|
-      enchantables_enchantments.find_or_create_by!(
-        enchantment_id: model.enchantment_id,
-        strength: model.strength,
-      ) {|new_model| new_model.added_automatically = true }
-    end
+  def canonical_table
+    'canonical_armors'
   end
 
-  def clear_canonical_armor
-    self.canonical_armor_id = nil
-    remove_automatically_added_enchantments!
+  def canonical_model_id
+    canonical_armor_id
+  end
+
+  def canonical_model_id_changed?
+    canonical_armor_id_changed?
+  end
+
+  alias_method :canonical_model=, :canonical_armor=
+
+  def set_values_from_canonical
+    return if canonical_model.nil?
+    return unless canonical_model_id_changed?
+
+    self.name = canonical_model.name # in case casing differs
+    self.magical_effects = canonical_model.magical_effects
+    self.unit_weight = canonical_model.unit_weight
+    self.weight = canonical_model.weight
+
+    set_enchantments if persisted?
   end
 
   def canonical_model_matches?
@@ -122,10 +74,6 @@ class Armor < ApplicationRecord
     return false unless magical_effects.nil? || magical_effects.casecmp(canonical_model.magical_effects)&.zero?
 
     true
-  end
-
-  def remove_automatically_added_enchantments!
-    enchantables_enchantments.added_automatically.find_each(&:destroy!)
   end
 
   def attributes_to_match
--- app/models/jewelry_item.rb
+++ app/models/jewelry_item.rb
@@ -1,21 +1,13 @@
 # frozen_string_literal: true
 
-class JewelryItem < ApplicationRecord
-  belongs_to :game
+class JewelryItem < EnchantableInGameItem
   belongs_to :canonical_jewelry_item,
              optional: true,
              inverse_of: :jewelry_items,
              class_name: 'Canonical::JewelryItem'
 
-  has_many :enchantables_enchantments,
-           dependent: :destroy,
-           as: :enchantable
-  has_many :enchantments,
-           -> { select 'enchantments.*, enchantables_enchantments.strength as strength' },
-           through: :enchantables_enchantments,
-           source: :enchantment
-
   validates :name, presence: true
+
   validates :unit_weight,
             allow_blank: true,
             numericality: {
@@ -24,9 +16,6 @@ class JewelryItem < ApplicationRecord
 
   validate :ensure_match_exists
   validate :validate_unique_canonical
-
-  before_validation :set_canonical_jewelry_item
-  after_create :set_enchantments, if: -> { canonical_jewelry_item.present? }
 
   DOES_NOT_MATCH = "doesn't match any jewelry item that exists in Skyrim"
   DUPLICATE_MATCH = 'is a duplicate of a unique in-game item'
@@ -39,40 +28,29 @@ class JewelryItem < ApplicationRecord
     canonical_jewelry_item
   end
 
-  def canonical_models
-    return Canonical::JewelryItem.where(id: canonical_jewelry_item_id) if canonical_model_matches?
-
-    query = 'name ILIKE :name'
-    query += ' AND magical_effects ILIKE :magical_effects' if magical_effects.present?
-
-    canonicals = Canonical::JewelryItem.where(query, name:, magical_effects:)
-    canonicals = canonicals.where(**attributes_to_match) if attributes_to_match.any?
-
-    return canonicals if enchantments.none?
-
-    enchantables_enchantments.added_manually.each do |join_model|
-      canonicals = if join_model.strength.present?
-                     canonicals.left_outer_joins(:enchantables_enchantments).where(
-                       '(enchantables_enchantments.enchantment_id = :enchantment_id AND enchantables_enchantments.strength = :strength) OR canonical_jewelry_items.enchantable = true',
-                       enchantment_id: join_model.enchantment_id,
-                       strength: join_model.strength,
-                     )
-                   else
-                     canonicals.left_outer_joins(:enchantables_enchantments).where(
-                       '(enchantables_enchantments.enchantment_id = :enchantment_id AND enchantables_enchantments.strength IS NULL) OR canonical_jewelry_items.enchantable = true',
-                       enchantment_id: join_model.enchantment_id,
-                     )
-                   end
-    end
-
-    canonicals.uniq
-  end
-
   def jewelry_type
     canonical_model&.jewelry_type
   end
 
   private
+
+  def canonical_class
+    Canonical::JewelryItem
+  end
+
+  def canonical_table
+    'canonical_jewelry_items'
+  end
+
+  def canonical_model_id
+    canonical_jewelry_item_id
+  end
+
+  def canonical_model_id_changed?
+    canonical_jewelry_item_id_changed?
+  end
+
+  alias_method :canonical_model=, :canonical_jewelry_item=
 
   def ensure_match_exists
     return if canonical_models.any?
@@ -80,20 +58,14 @@ class JewelryItem < ApplicationRecord
     errors.add(:base, DOES_NOT_MATCH)
   end
 
-  def set_canonical_jewelry_item
-    canonicals = canonical_models
+  def set_values_from_canonical
+    return if canonical_model.nil?
 
-    unless canonicals.count == 1
-      clear_canonical_jewelry_item
-      return
-    end
+    self.name = canonical_model.name
+    self.unit_weight = canonical_model.unit_weight
+    self.magical_effects = canonical_model.magical_effects
 
-    self.canonical_jewelry_item = canonicals.first
-    self.name = canonical_jewelry_item.name
-    self.unit_weight = canonical_jewelry_item.unit_weight
-    self.magical_effects = canonical_jewelry_item.magical_effects
-
-    set_enchantments if persisted? && canonical_jewelry_item_id_changed?
+    set_enchantments if persisted? && canonical_model_id_changed?
   end
 
   def validate_unique_canonical
@@ -107,24 +79,6 @@ class JewelryItem < ApplicationRecord
     errors.add(:base, DUPLICATE_MATCH)
   end
 
-  def set_enchantments
-    return if canonical_jewelry_item.enchantments.empty?
-
-    remove_automatically_added_enchantments!
-
-    canonical_jewelry_item.enchantables_enchantments.each do |model|
-      enchantables_enchantments.find_or_create_by!(
-        enchantment_id: model.enchantment_id,
-        strength: model.strength,
-      ) {|new_model| new_model.added_automatically = true }
-    end
-  end
-
-  def clear_canonical_jewelry_item
-    self.canonical_jewelry_item_id = nil
-    remove_automatically_added_enchantments!
-  end
-
   def canonical_model_matches?
     return false if canonical_model.nil?
     return false unless name.casecmp(canonical_model.name).zero?
@@ -132,10 +86,6 @@ class JewelryItem < ApplicationRecord
     return false unless unit_weight.nil? || unit_weight == canonical_model.unit_weight
 
     true
-  end
-
-  def remove_automatically_added_enchantments!
-    enchantables_enchantments.added_automatically.find_each(&:destroy!)
   end
 
   def attributes_to_match
