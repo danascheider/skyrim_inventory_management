64a4e1efbcc7a04eb1c8790080fc5c0f235e7d2f
Dana Scheider <dana.scheider@gmail.com>
2024-03-31 08:34:38 +1100

    Migration to add new fields to canonical potions

---
+++ db/migrate/20240330213057_add_new_fields_to_canonical_potions.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class AddNewFieldsToCanonicalPotions < ActiveRecord::Migration[7.1]
+  def change
+    add_column :canonical_potions,
+               :add_on,
+               :string
+    add_column :canonical_potions,
+               :collectible,
+               :boolean
+    add_column :canonical_potions,
+               :max_quantity,
+               :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_24_035111) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_30_213057) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -197,6 +197,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_24_035111) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "quest_reward", default: false
+    t.string "add_on"
+    t.boolean "collectible"
+    t.integer "max_quantity"
     t.index ["item_code"], name: "index_canonical_potions_on_item_code", unique: true
   end
 
