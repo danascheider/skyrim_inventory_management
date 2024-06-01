8cf9ae7a3722b60986bf8ff1b486dd8128331600
Dana Scheider <dana.scheider@gmail.com>
2024-06-02 09:42:24 +1000

    Update application and test code to validate new columns

--- spec/models/canonical/ingredient_spec.rb
+++ spec/models/canonical/ingredient_spec.rb
@@ -4,12 +4,18 @@ require 'rails_helper'
 
 RSpec.describe Canonical::Ingredient, type: :model do
   describe 'validations' do
+    subject(:validate) { model.validate }
+
+    let(:model) { build(:canonical_ingredient) }
+
     it 'is valid with valid attributes' do
       ingredient = described_class.new(
         name: 'Skeever Tail',
         item_code: 'foo',
         ingredient_type: 'common',
         unit_weight: 1,
+        add_on: 'base',
+        collectible: true,
         purchasable: true,
         purchase_requires_perk: false,
         unique_item: false,
@@ -21,80 +27,133 @@ RSpec.describe Canonical::Ingredient, type: :model do
 
     describe 'name' do
       it 'must be present' do
-        model = build(:canonical_ingredient, name: nil)
+        model.name = nil
+        validate
 
-        model.validate
         expect(model.errors[:name]).to include "can't be blank"
       end
     end
 
     describe 'item_code' do
       it 'must be present' do
-        model = build(:canonical_ingredient, item_code: nil)
+        model.item_code = nil
+        validate
 
-        model.validate
         expect(model.errors[:item_code]).to include "can't be blank"
       end
 
       it 'must be unique' do
         create(:canonical_ingredient, item_code: 'foo')
-        model = build(:canonical_ingredient, item_code: 'foo')
+        model.item_code = 'foo'
+        validate
 
-        model.validate
         expect(model.errors[:item_code]).to include 'must be unique'
       end
     end
 
     describe 'ingredient_type' do
       it 'must have one of the valid values' do
-        model = build(:canonical_ingredient, ingredient_type: 'unique')
+        model.ingredient_type = 'unique'
+        validate
 
-        model.validate
         expect(model.errors[:ingredient_type]).to include 'must be "common", "uncommon", "rare", or "Solstheim"'
       end
 
       it 'can be blank if purchasable is false' do
-        model = build(:canonical_ingredient, ingredient_type: nil, purchasable: false, purchase_requires_perk: nil)
+        model.purchasable = false
+        model.purchase_requires_perk = nil
+        model.ingredient_type = nil
 
         expect(model).to be_valid
       end
 
       it 'must be blank if purchasable is false' do
-        model = build(:canonical_ingredient, purchasable: false, ingredient_type: 'uncommon')
+        model.purchasable = false
+        model.purchase_requires_perk = nil
+        model.ingredient_type = 'uncommon'
+        validate
 
-        model.validate
         expect(model.errors[:ingredient_type]).to include 'can only be set for purchasable ingredients'
       end
 
       it "can't be blank if purchasable is true" do
-        model = build(:canonical_ingredient, purchasable: true, ingredient_type: nil)
+        model.purchasable = true
+        model.purchase_requires_perk = false
+        model.ingredient_type = nil
+        validate
 
-        model.validate
         expect(model.errors[:ingredient_type]).to include "can't be blank for purchasable ingredients"
       end
     end
 
     describe 'unit_weight' do
       it 'must be present' do
-        ingredient = described_class.new(name: 'Thistle Branch', item_code: 'foo')
+        model.unit_weight = nil
+        validate
 
-        ingredient.validate
-        expect(ingredient.errors[:unit_weight]).to include "can't be blank"
+        expect(model.errors[:unit_weight]).to include "can't be blank"
       end
 
       it "can't be less than zero" do
-        ingredient = build(:canonical_ingredient, unit_weight: -0.5)
+        model.unit_weight = -1
+        validate
 
-        ingredient.validate
-        expect(ingredient.errors[:unit_weight]).to include 'must be greater than or equal to 0'
+        expect(model.errors[:unit_weight]).to include 'must be greater than or equal to 0'
+      end
+    end
+
+    describe 'add_on' do
+      it 'must be present' do
+        model.add_on = nil
+        validate
+
+        expect(model.errors[:add_on]).to include "can't be blank"
+      end
+
+      it 'must be a valid add-on or DLC' do
+        model.add_on = 'fishing'
+        validate
+
+        expect(model.errors[:add_on]).to include 'must be a SIM-supported add-on or DLC'
+      end
+    end
+
+    describe 'max_quantity' do
+      it 'can be nil' do
+        model.max_quantity = nil
+
+        expect(model).to be_valid
+      end
+
+      it 'must be greater than zero' do
+        model.max_quantity = 0
+        validate
+
+        expect(model.errors[:max_quantity]).to include 'must be an integer of at least 1'
+      end
+
+      it 'must be an integer' do
+        model.max_quantity = 1.2
+        validate
+
+        expect(model.errors[:max_quantity]).to include 'must be an integer of at least 1'
+      end
+    end
+
+    describe 'collectible' do
+      it 'must be true or false' do
+        model.collectible = nil
+        validate
+
+        expect(model.errors[:collectible]).to include 'must be true or false'
       end
     end
 
     describe 'purchasable' do
       it 'must be true or false' do
-        model = build(:canonical_ingredient, purchasable: nil)
+        model.purchasable = nil
+        validate
 
-        model.validate
         expect(model.errors[:purchasable]).to include 'must be true or false'
       end
     end
@@ -108,7 +167,9 @@ RSpec.describe Canonical::Ingredient, type: :model do
       # This spec tests whether the model IS valid when purchase_requires_perk is nil and purchasable is false.
       # The next spec tests the validation error/message.
       it 'can be NULL if purchasable is false' do
-        model = build(:canonical_ingredient, ingredient_type: nil, purchasable: false, purchase_requires_perk: nil)
+        model.purchasable = false
+        model.purchase_requires_perk = nil
+        model.ingredient_type = nil
 
         expect(model).to be_valid
       end
@@ -116,50 +177,53 @@ RSpec.describe Canonical::Ingredient, type: :model do
       # The above spec tests whether the model is valid when the value is nil and purchasable is false. This
       # spec tests the validation error/message.
       it 'must be NULL if purchasable is false' do
-        model = build(:canonical_ingredient, ingredient_type: nil, purchasable: false, purchase_requires_perk: false)
+        model.purchasable = false
+        model.purchase_requires_perk = true
+        validate
 
-        model.validate
         expect(model.errors[:purchase_requires_perk]).to include "can't be set if purchasable is false"
       end
 
       it 'must be set if purchasable is true' do
-        model = build(:canonical_ingredient, ingredient_type: 'common', purchasable: true, purchase_requires_perk: nil)
+        model.purchasable = true
+        model.purchase_requires_perk = nil
+        validate
 
-        model.validate
         expect(model.errors[:purchase_requires_perk]).to include 'must be true or false if purchasable is true'
       end
     end
 
     describe 'unique_item' do
       it 'must be true or false' do
-        model = build(:canonical_ingredient, unique_item: nil)
+        model.unique_item = nil
+        validate
 
-        model.validate
         expect(model.errors[:unique_item]).to include 'must be true or false'
       end
     end
 
     describe 'rare_item' do
       it 'must be true or false' do
-        model = build(:canonical_ingredient, rare_item: nil)
+        model.rare_item = nil
+        validate
 
-        model.validate
         expect(model.errors[:rare_item]).to include 'must be true or false'
       end
 
       it 'must be true if the ingredient is unique' do
-        model = build(:canonical_ingredient, unique_item: true, rare_item: false)
+        model.unique_item = true
+        model.rare_item = false
+        validate
 
-        model.validate
         expect(model.errors[:rare_item]).to include 'must be true if item is unique'
       end
     end
 
     describe 'quest_item' do
       it 'must be true or false' do
-        model = build(:canonical_ingredient, quest_item: nil)
+        model.quest_item = nil
+        validate
 
-        model.validate
         expect(model.errors[:quest_item]).to include 'must be true or false'
       end
     end
