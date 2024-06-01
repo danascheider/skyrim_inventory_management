a06f9dc345e34ed45797ad1d769fe0f1a82d23db
Dana Scheider <dana.scheider@gmail.com>
2024-06-01 13:10:35 +1000

    Add new columns to canonical_clothing_items table

---
+++ db/migrate/20240601023951_add_new_fields_to_canonical_clothing_items.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class AddNewFieldsToCanonicalClothingItems < ActiveRecord::Migration[7.1]
+  def change
+    add_column :canonical_clothing_items,
+               :add_on,
+               :string
+    add_column :canonical_clothing_items,
+               :max_quantity,
+               :integer
+    add_column :canonical_clothing_items,
+               :collectible,
+               :boolean
+  end
+end
