b08648640665f6fa2f1e6bb2b20a2c05a4c631f1
Dana Scheider <dana.scheider@gmail.com>
2024-08-23 20:53:41 +1000

    Add add_on column to enchantments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_23_050136) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_23_103752) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -373,6 +373,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_23_050136) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "school"
+    t.string "add_on"
     t.index ["name"], name: "index_enchantments_on_name", unique: true
   end
 
