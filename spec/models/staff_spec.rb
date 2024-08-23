0a3a80b530e64893a7e1a057a4ccc572de575f85
Dana Scheider <dana.scheider@gmail.com>
2024-08-23 14:41:12 +1000

    Update JSON files to include new data

--- spec/models/staff_spec.rb
+++ spec/models/staff_spec.rb
@@ -55,6 +55,7 @@ RSpec.describe Staff, type: :model do
         let(:canonical_staff) do
           create(
             :canonical_staff,
+            max_quantity: 1,
             unique_item: true,
             rare_item: true,
           )
