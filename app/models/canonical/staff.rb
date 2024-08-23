9b1e9fdef03c9475997cff2165b20364e0900977
Dana Scheider <dana.scheider@gmail.com>
2024-08-23 12:28:04 +1000

    Add validations for new Canonical::Staff attributes

--- app/models/canonical/staff.rb
+++ app/models/canonical/staff.rb
@@ -28,6 +28,13 @@ module Canonical
     validates :item_code,
               presence: true,
               uniqueness: { message: 'must be unique' }
+    validates :max_quantity,
+              presence: false,
+              numericality: {
+                greater_than: 0,
+                only_integer: true,
+                allow_nil: true,
+              }
     validates :unit_weight,
               presence: true,
               numericality: { greater_than_or_equal_to: 0 }
@@ -42,6 +49,17 @@ module Canonical
                 in: Skyrim::MAGIC_SCHOOLS,
                 message: 'must be a valid school of magic',
                 allow_blank: true,
+              }
+    validates :add_on,
+              presence: true,
+              inclusion: {
+                in: SUPPORTED_ADD_ONS,
+                message: UNSUPPORTED_ADD_ON_MESSAGE,
+              }
+    validates :collectible,
+              inclusion: {
+                in: BOOLEAN_VALUES,
+                message: BOOLEAN_VALIDATION_MESSAGE,
               }
     validates :daedric,
               inclusion: {
@@ -74,7 +92,7 @@ module Canonical
                 message: BOOLEAN_VALIDATION_MESSAGE,
               }
 
-    validate :validate_unique_item_also_rare, if: -> { unique_item == true }
+    validate :validate_uniqueness, if: -> { unique_item == true || max_quantity == 1 }
 
     before_validation :upcase_item_code, if: -> { item_code_changed? }
 
@@ -84,7 +102,9 @@ module Canonical
 
     private
 
-    def validate_unique_item_also_rare
+    def validate_uniqueness
+      errors.add(:unique_item, 'must be true if max quantity is 1') if max_quantity == 1 && !unique_item
+      errors.add(:unique_item, 'must correspond to a max quantity of 1') if unique_item == true && max_quantity != 1
       errors.add(:rare_item, 'must be true if item is unique') unless rare_item == true
     end
 
--- spec/models/canonical/staff_spec.rb
+++ spec/models/canonical/staff_spec.rb
@@ -4,158 +4,205 @@ require 'rails_helper'
 
 RSpec.describe Canonical::Staff, type: :model do
   describe 'validations' do
+    subject(:validate) { staff.validate }
+
+    let(:staff) { build(:canonical_staff) }
+
     describe 'name' do
       it "can't be blank" do
-        model = build(:canonical_staff, name: nil)
-
-        model.validate
-        expect(model.errors[:name]).to include "can't be blank"
+        staff.name = nil
+        validate
+        expect(staff.errors[:name]).to include "can't be blank"
       end
     end
 
     describe 'item_code' do
       it "can't be blank" do
-        model = build(:canonical_staff, item_code: nil)
-
-        model.validate
-        expect(model.errors[:item_code]).to include "can't be blank"
+        staff.item_code = nil
+        validate
+        expect(staff.errors[:item_code]).to include "can't be blank"
       end
 
       it 'must be unique' do
         create(:canonical_staff, item_code: 'foobar')
-        model = build(:canonical_staff, item_code: 'foobar')
-
-        model.validate
-        expect(model.errors[:item_code]).to include 'must be unique'
+        staff.item_code = 'foobar'
+        validate
+        expect(staff.errors[:item_code]).to include 'must be unique'
       end
     end
 
     describe 'unit_weight' do
       it "can't be blank" do
-        model = build(:canonical_staff, unit_weight: nil)
-
-        model.validate
-        expect(model.errors[:unit_weight]).to include "can't be blank"
+        staff.unit_weight = nil
+        validate
+        expect(staff.errors[:unit_weight]).to include "can't be blank"
       end
 
       it 'must be a number' do
-        model = build(:canonical_staff, unit_weight: 'foobar')
-
-        model.validate
-        expect(model.errors[:unit_weight]).to include 'is not a number'
+        staff.unit_weight = 'foobar'
+        validate
+        expect(staff.errors[:unit_weight]).to include 'is not a number'
       end
 
       it 'must be at least zero' do
-        model = build(:canonical_staff, unit_weight: -2.2)
-
-        model.validate
-        expect(model.errors[:unit_weight]).to include 'must be greater than or equal to 0'
+        staff.unit_weight = -2.2
+        validate
+        expect(staff.errors[:unit_weight]).to include 'must be greater than or equal to 0'
       end
     end
 
     describe 'base_damage' do
       it "can't be blank" do
-        model = build(:canonical_staff, base_damage: nil)
-
-        model.validate
-        expect(model.errors[:base_damage]).to include "can't be blank"
+        staff.base_damage = nil
+        validate
+        expect(staff.errors[:base_damage]).to include "can't be blank"
       end
 
       it 'must be a number' do
-        model = build(:canonical_staff, base_damage: 'foobar')
-
-        model.validate
-        expect(model.errors[:base_damage]).to include 'is not a number'
+        staff.base_damage = 'foobar'
+        validate
+        expect(staff.errors[:base_damage]).to include 'is not a number'
       end
 
       it 'must be at least zero' do
-        model = build(:canonical_staff, base_damage: -1)
-
-        model.validate
-        expect(model.errors[:base_damage]).to include 'must be greater than or equal to 0'
+        staff.base_damage = -1
+        validate
+        expect(staff.errors[:base_damage]).to include 'must be greater than or equal to 0'
       end
 
       it 'must be an integer' do
-        model = build(:canonical_staff, base_damage: 8.2)
-
-        model.validate
-        expect(model.errors[:base_damage]).to include 'must be an integer'
+        staff.base_damage = 8.2
+        validate
+        expect(staff.errors[:base_damage]).to include 'must be an integer'
       end
     end
 
     describe 'school' do
       it 'can be blank' do
-        model = build(:canonical_staff, school: nil)
-
-        expect(model).to be_valid
+        staff.school = nil
+        expect(staff).to be_valid
       end
 
       it 'must be an actual school' do
-        model = build(:canonical_staff, school: 'Hard Knocks')
+        staff.school = 'Hard Knocks'
+        validate
+        expect(staff.errors[:school]).to include 'must be a valid school of magic'
+      end
+    end
 
-        model.validate
-        expect(model.errors[:school]).to include 'must be a valid school of magic'
+    describe 'max_quantity' do
+      it 'can be blank' do
+        staff.max_quantity = nil
+        expect(staff).to be_valid
+      end
+
+      it 'must be greater than zero' do
+        staff.max_quantity = 0
+        validate
+        expect(staff.errors[:max_quantity]).to include 'must be greater than 0'
+      end
+
+      it 'must be an integer' do
+        staff.max_quantity = 7.45
+        validate
+        expect(staff.errors[:max_quantity]).to include 'must be an integer'
+      end
+    end
+
+    describe 'add_on' do
+      it "can't be blank" do
+        staff.add_on = nil
+        validate
+        expect(staff.errors[:add_on]).to include "can't be blank"
+      end
+
+      it 'must be a supported add-on' do
+        staff.add_on = 'fishing'
+        validate
+        expect(staff.errors[:add_on]).to include 'must be a SIM-supported add-on or DLC'
+      end
+    end
+
+    describe 'collectible' do
+      it "can't be blank" do
+        staff.collectible = nil
+        validate
+        expect(staff.errors[:collectible]).to include 'must be true or false'
       end
     end
 
     describe 'daedric' do
       it "can't be blank" do
-        model = build(:canonical_staff, daedric: nil)
-
-        model.validate
-        expect(model.errors[:daedric]).to include 'must be true or false'
+        staff.daedric = nil
+        validate
+        expect(staff.errors[:daedric]).to include 'must be true or false'
       end
     end
 
     describe 'purchasable' do
       it "can't be blank" do
-        model = build(:canonical_staff, purchasable: nil)
-
-        model.validate
-        expect(model.errors[:purchasable]).to include 'must be true or false'
+        staff.purchasable = nil
+        validate
+        expect(staff.errors[:purchasable]).to include 'must be true or false'
       end
     end
 
     describe 'unique_item' do
       it "can't be blank" do
-        model = build(:canonical_staff, unique_item: nil)
+        staff.unique_item = nil
+        validate
+        expect(staff.errors[:unique_item]).to include 'must be true or false'
+      end
 
-        model.validate
-        expect(model.errors[:unique_item]).to include 'must be true or false'
+      it 'must be true if max quantity is 1' do
+        staff.max_quantity = 1
+        staff.unique_item = false
+
+        validate
+
+        expect(staff.errors[:unique_item]).to include 'must be true if max quantity is 1'
+      end
+
+      it 'must be false if max quantity is not 1' do
+        staff.max_quantity = 2
+        staff.unique_item = true
+
+        validate
+
+        expect(staff.errors[:unique_item]).to include 'must correspond to a max quantity of 1'
       end
     end
 
     describe 'rare_item' do
       it "can't be blank" do
-        model = build(:canonical_staff, rare_item: nil)
-
-        model.validate
-        expect(model.errors[:rare_item]).to include 'must be true or false'
+        staff.rare_item = nil
+        validate
+        expect(staff.errors[:rare_item]).to include 'must be true or false'
       end
 
       it 'must be true if the item is unique' do
-        model = build(:canonical_staff, unique_item: true, rare_item: false)
+        staff.unique_item = true
+        staff.rare_item = false
 
-        model.validate
-        expect(model.errors[:rare_item]).to include 'must be true if item is unique'
+        validate
+
+        expect(staff.errors[:rare_item]).to include 'must be true if item is unique'
       end
     end
 
     describe 'quest_item' do
       it "can't be blank" do
-        model = build(:canonical_staff, quest_item: nil)
-
-        model.validate
-        expect(model.errors[:quest_item]).to include 'must be true or false'
+        staff.quest_item = nil
+        validate
+        expect(staff.errors[:quest_item]).to include 'must be true or false'
       end
     end
 
     describe 'leveled' do
       it "can't be blank" do
-        model = build(:canonical_staff, leveled: nil)
-
-        model.validate
-        expect(model.errors[:leveled]).to include 'must be true or false'
+        staff.leveled = nil
+        validate
+        expect(staff.errors[:leveled]).to include 'must be true or false'
       end
     end
   end
