37fb1d83294bcb20305240d467279b6f81058890
Dana Scheider <dana.scheider@gmail.com>
2024-08-24 15:31:02 +1000

    Add add_on column to powers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_23_222345) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_24_053020) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -490,6 +490,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_23_222345) do
     t.string "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "add_on"
     t.index ["name"], name: "index_powers_on_name", unique: true
   end
 
